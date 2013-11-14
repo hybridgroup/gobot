3adbd3c9058083eb5dc7a26a51314bc421120a5c
deadprogram <ron@hybridgroup.com>
2013-11-13 20:49:57 -0800

    Add Sphero example

---
+++ examples/sphero.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"github.com/hybridgroup/gobot"
+	"github.com/hybridgroup/gobot-sphero"
+)
+
+func main() {
+
+	spheroAdaptor := new(gobotSphero.SpheroAdaptor)
+	spheroAdaptor.Name = "Sphero"
+	spheroAdaptor.Port = "127.0.0.1:4560"
+
+	sphero := gobotSphero.NewSphero(spheroAdaptor)
+	sphero.Name = "Sphero"
+
+	connections := []interface{}{
+		spheroAdaptor,
+	}
+	devices := []interface{}{
+		sphero,
+	}
+
+	work := func() {
+		gobot.Every("2s", func() {
+			sphero.Roll(100, uint16(gobot.Random(0, 360)))
+		})
+	}
+
+	robot := gobot.Robot{
+		Connections: connections,
+		Devices:     devices,
+		Work:        work,
+	}
+
+	robot.Start()
+}
