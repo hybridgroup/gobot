a0ac5f1f9f3b7ca7aa4a316f25693c57d0612551
Adrian Zankich <adrian@hybridgroup.com>
2013-11-23 09:15:44 -0800

    Add master example

---
+++ examples/sphero_master.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+  "github.com/hybridgroup/gobot"
+  "github.com/hybridgroup/gobot-sphero"
+)
+
+func main() {
+  bot := gobot.NewGobot()
+
+  spheros := map[string]string{
+    "Sphero-BPO": "127.0.0.1:4560",
+  }
+
+  for name, port := range spheros {
+    spheroAdaptor := new(gobotSphero.SpheroAdaptor)
+    spheroAdaptor.Name = "sphero"
+    spheroAdaptor.Port = port
+
+    sphero := gobotSphero.NewSphero(spheroAdaptor)
+    sphero.Name = "sphero"
+    sphero.Interval = "0.5s"
+
+    work := func(){
+      sphero.SetRGB(uint8(255), uint8(0), uint8(0))
+    }
+
+    bot.Robots = append(bot.Robots, gobot.Robot {
+      Name: name, 
+      Connections: []interface{} { spheroAdaptor }, 
+      Devices: []interface{} { sphero }, 
+      Work: work,
+    })
+  }
+
+  bot.Robots = append(bot.Robots, gobot.Robot {
+    Work: func(){
+      sphero := bot.FindRobot("Sphero-BPO")
+      gobot.Every("1s", func () {
+        gobot.Call(sphero.Device("sphero").Driver, "SetRGB", uint8(gobot.Rand(255)), uint8(gobot.Rand(255)), uint8(gobot.Rand(255)))
+      })
+    },
+  })
+
+  bot.Start()
+}
