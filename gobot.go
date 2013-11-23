e6ef542a98cc37fede07fdd18bbaf574d6bbbe23
Adrian Zankich <adrian@hybridgroup.com>
2013-11-23 09:26:54 -0800

    Clean up variables

--- gobot.go
+++ gobot.go
@@ -3,18 +3,15 @@ package gobot
 import "time"
 
 type Gobot struct {
-	//  Master *Master
 	Robots []Robot
 }
 
 func NewGobot() *Gobot {
 	g := new(Gobot)
-	//  g.Master = new(Master)
 	return g
 }
 
 func (g *Gobot) Start() {
-	//  g.Master.robots = g.Robots
 	for s := range g.Robots {
 		go g.Robots[s].Start()
 	}
@@ -24,32 +21,32 @@ func (g *Gobot) Start() {
 	}
 }
 
-func (m *Gobot) FindRobot(name string) *Robot {
-	for s := range m.Robots {
-		if m.Robots[s].Name == name {
-			return &m.Robots[s]
+func (g *Gobot) FindRobot(name string) *Robot {
+	for s := range g.Robots {
+		if g.Robots[s].Name == name {
+			return &g.Robots[s]
 		}
 	}
 	return nil
 }
-func (m *Gobot) FindRobotDevice(name string, device string) *Device {
-	for r := range m.Robots {
-		if m.Robots[r].Name == name {
-			for d := range m.Robots[r].devices {
-				if m.Robots[r].devices[d].Name == device {
-					return m.Robots[r].devices[d]
+func (g *Gobot) FindRobotDevice(name string, device string) *Device {
+	for r := range g.Robots {
+		if g.Robots[r].Name == name {
+			for d := range g.Robots[r].devices {
+				if g.Robots[r].devices[d].Name == device {
+					return g.Robots[r].devices[d]
 				}
 			}
 		}
 	}
 	return nil
 }
-func (m *Gobot) FindRobotConnection(name string, connection string) *Connection {
-	for r := range m.Robots {
-		if m.Robots[r].Name == name {
-			for c := range m.Robots[r].connections {
-				if m.Robots[r].connections[c].Name == connection {
-					return m.Robots[r].connections[c]
+func (g *Gobot) FindRobotConnection(name string, connection string) *Connection {
+	for r := range g.Robots {
+		if g.Robots[r].Name == name {
+			for c := range g.Robots[r].connections {
+				if g.Robots[r].connections[c].Name == connection {
+					return g.Robots[r].connections[c]
 				}
 			}
 		}
