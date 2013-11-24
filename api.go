f1ac7a90737b79cd7b0521c374f86629aacc85ef
Adrian Zankich <adrian@hybridgroup.com>
2013-11-24 14:56:13 -0800

    go fmt examples

--- api.go
+++ api.go
@@ -31,13 +31,13 @@ func Api(bot *Gobot) {
 
 	m.Post("/robots/:robotname/devices/:devicename/commands/:command", func(params martini.Params, res http.ResponseWriter, req *http.Request) string {
 		decoder := json.NewDecoder(req.Body)
-		var response_hash map[string]interface{}
-		decoder.Decode(&response_hash)
+		var body map[string]interface{}
+		decoder.Decode(&body)
 		robot := bot.FindRobotDevice(params["robotname"], params["devicename"])
 		commands := robot.Commands().([]string)
 		for command := range commands {
 			if commands[command] == params["command"] {
-				ret := Call(robot.Driver, params["command"], response_hash)
+				ret := Call(robot.Driver, params["command"], body)
 				return toJson(map[string]interface{}{"results": ret})
 			}
 		}
