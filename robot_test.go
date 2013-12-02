305bee8239a3421fff3ca5f4229677763181390c
deadprogram <ron@hybridgroup.com>
2013-12-01 22:20:39 -0800

    Nicer format

--- robot_test.go
+++ robot_test.go
@@ -1,46 +1,46 @@
 package gobot
 
 import (
-    . "github.com/onsi/ginkgo"
-    . "github.com/onsi/gomega"
+  . "github.com/onsi/ginkgo"
+  . "github.com/onsi/gomega"
 )
 
 var _ = Describe("Robot", func() {
 
-var (
-        someRobot Robot
-    )
+  var (
+    someRobot Robot
+  )
 
-    BeforeEach(func() {
-        someRobot = Robot{
-          Work: func() {
-          },
-        }
-    })
+  BeforeEach(func() {
+    someRobot = Robot{
+      Work: func() {
+      },
+    }
+  })
 
-    Describe("Robot", func() {
-        Context("when valid", func() {
-            PIt("should Start", func() {
-                Expect(true)
-            })
-            PIt("should initConnections", func() {
-                Expect(true)
-            })
-            PIt("should initDevices", func() {
-                Expect(true)
-            })
-            PIt("should startConnections", func() {
-                Expect(true)
-            })
-            PIt("should startDevices", func() {
-                Expect(true)
-            })            
-            PIt("should GetDevices", func() {
-                Expect(true)
-            })            
-            PIt("should GetDevice", func() {
-                Expect(true)
-            })            
-        })
+  Describe("Robot", func() {
+    Context("when valid", func() {
+      PIt("should Start", func() {
+        Expect(true)
+      })
+      PIt("should initConnections", func() {
+        Expect(true)
+      })
+      PIt("should initDevices", func() {
+        Expect(true)
+      })
+      PIt("should startConnections", func() {
+        Expect(true)
+      })
+      PIt("should startDevices", func() {
+        Expect(true)
+      })
+      PIt("should GetDevices", func() {
+        Expect(true)
+      })
+      PIt("should GetDevice", func() {
+        Expect(true)
+      })
     })
+  })
 })
