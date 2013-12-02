d76fe47574931616f4fe2e0986f779887a1f07ab
deadprogram <ron@hybridgroup.com>
2013-12-01 22:16:02 -0800

    Pending tests for Robot

---
+++ robot_test.go
@@ -0,0 +1,46 @@
+package gobot
+
+import (
+    . "github.com/onsi/ginkgo"
+    . "github.com/onsi/gomega"
+)
+
+var _ = Describe("Robot", func() {
+
+var (
+        someRobot Robot
+    )
+
+    BeforeEach(func() {
+        someRobot = Robot{
+          Work: func() {
+          },
+        }
+    })
+
+    Describe("Robot", func() {
+        Context("when valid", func() {
+            PIt("should Start", func() {
+                Expect(true)
+            })
+            PIt("should initConnections", func() {
+                Expect(true)
+            })
+            PIt("should initDevices", func() {
+                Expect(true)
+            })
+            PIt("should startConnections", func() {
+                Expect(true)
+            })
+            PIt("should startDevices", func() {
+                Expect(true)
+            })            
+            PIt("should GetDevices", func() {
+                Expect(true)
+            })            
+            PIt("should GetDevice", func() {
+                Expect(true)
+            })            
+        })
+    })
+})
