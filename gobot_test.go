819ec4f709fd6f41af928b170d2a07d60db4bf15
Adrian Zankich <adrian@hybridgroup.com>
2014-05-03 03:22:22 -0700

    Gobot tests are green again

--- gobot_test.go
+++ gobot_test.go
@@ -6,32 +6,49 @@ import (
 	"os"
 )
 
-var _ = Describe("Master", func() {
+var _ = Describe("Gobot", func() {
 	var (
-		m *Master
+		g *Gobot
 	)
 
 	BeforeEach(func() {
-		m = NewMaster()
-		m.trap = func(c chan os.Signal) {
+		g = NewGobot()
+		g.trap = func(c chan os.Signal) {
 			c <- os.Interrupt
 		}
-		m.Robots = []*Robot{
+		g.Robots = []*Robot{
 			newTestRobot("Robot 1"),
 			newTestRobot("Robot 2"),
 			newTestRobot("Robot 3"),
 		}
-		m.Api = NewApi()
-		m.Api.startFunc = func(m *api) {}
-		m.Start()
+		g.Start()
 	})
 
 	Context("when valid", func() {
 		It("should Find the specific robot", func() {
-			Expect(m.FindRobot("Robot 1").Name).To(Equal("Robot 1"))
+			Expect(g.Robot("Robot 1").Name).To(Equal("Robot 1"))
 		})
 		It("should return nil if Robot doesn't exist", func() {
-			Expect(m.FindRobot("Robot 4")).To(BeNil())
+			Expect(g.Robot("Robot 4")).To(BeNil())
 		})
+		It("Device should return nil if device doesn't exist", func() {
+			Expect(g.Robot("Robot 1").Device("Device 4")).To(BeNil())
+		})
+		It("Device should return device", func() {
+			Expect(g.Robot("Robot 1").Device("Device 1").Name).To(Equal("Device 1"))
+		})
+		It("Devices should return devices", func() {
+			Expect(len(g.Robot("Robot 1").Devices())).To(Equal(3))
+		})
+		It("Connection should return nil if connection doesn't exist", func() {
+			Expect(g.Robot("Robot 1").Connection("Connection 4")).To(BeNil())
+		})
+		It("Connection should return connection", func() {
+			Expect(g.Robot("Robot 1").Connection("Connection 1").Name).To(Equal("Connection 1"))
+		})
+		It("Connections should return connections", func() {
+			Expect(len(g.Robot("Robot 1").Connections())).To(Equal(3))
+		})
+
 	})
 })
