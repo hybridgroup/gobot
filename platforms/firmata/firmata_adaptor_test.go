f7a3ebbf3db2a4386db60bf4b64f855bdae42dd8
Adrian Zankich <adrian@hybridgroup.com>
2014-07-14 14:10:04 -0700

    Green tests

--- platforms/firmata/firmata_adaptor_test.go
+++ platforms/firmata/firmata_adaptor_test.go
@@ -3,6 +3,7 @@ package firmata
 import (
 	"fmt"
 	"testing"
+	"time"
 
 	"github.com/hybridgroup/gobot"
 )
@@ -10,13 +11,16 @@ import (
 func initTestFirmataAdaptor() *FirmataAdaptor {
 	a := NewFirmataAdaptor("board", "/dev/null")
 	a.connect = func(f *FirmataAdaptor) {
-		f.Board = newBoard(gobot.NullReadWriteCloser{})
-		f.Board.Events = append(f.Board.Events, event{Name: "firmware_query"})
-		f.Board.Events = append(f.Board.Events, event{Name: "capability_query"})
-		f.Board.Events = append(f.Board.Events, event{Name: "analog_mapping_query"})
+		f.board = newBoard(gobot.NullReadWriteCloser{})
+		f.board.initTimeInterval = 0 * time.Second
+		f.board.pins = make([]pin, 100)
+		f.board.events["digital_read_1"] = gobot.NewEvent()
+		f.board.events["analog_read_1"] = gobot.NewEvent()
+		gobot.Publish(f.board.events["firmware_query"], nil)
+		gobot.Publish(f.board.events["capability_query"], nil)
+		gobot.Publish(f.board.events["analog_mapping_query"], nil)
 	}
 	a.Connect()
-	a.Board.Pins = make([]pin, 100)
 	return a
 }
 
@@ -28,31 +32,38 @@ func TestFirmataAdaptorConnect(t *testing.T) {
 	a := initTestFirmataAdaptor()
 	gobot.Expect(t, a.Connect(), true)
 }
+
 func TestFirmataAdaptorInitServo(t *testing.T) {
 	a := initTestFirmataAdaptor()
 	a.InitServo()
 }
+
 func TestFirmataAdaptorServoWrite(t *testing.T) {
 	a := initTestFirmataAdaptor()
 	a.ServoWrite("1", 50)
 }
+
 func TestFirmataAdaptorPwmWrite(t *testing.T) {
 	a := initTestFirmataAdaptor()
 	a.PwmWrite("1", 50)
 }
+
 func TestFirmataAdaptorDigitalWrite(t *testing.T) {
 	a := initTestFirmataAdaptor()
 	a.DigitalWrite("1", 1)
 }
+
 func TestFirmataAdaptorDigitalRead(t *testing.T) {
 	a := initTestFirmataAdaptor()
 	// -1 on no data
 	gobot.Expect(t, a.DigitalRead("1"), -1)
 
 	pinNumber := "1"
-	a.Board.Events = append(a.Board.Events, event{Name: fmt.Sprintf("digital_read_%v", pinNumber), Data: []byte{0x01}})
+	gobot.Publish(a.board.events[fmt.Sprintf("digital_read_%v", pinNumber)],
+		[]byte{0x01})
 	gobot.Expect(t, a.DigitalRead(pinNumber), 0x01)
 }
+
 func TestFirmataAdaptorAnalogRead(t *testing.T) {
 	a := initTestFirmataAdaptor()
 	// -1 on no data
@@ -60,8 +71,19 @@ func TestFirmataAdaptorAnalogRead(t *testing.T) {
 
 	pinNumber := "1"
 	value := 133
-	a.Board.Events = append(a.Board.Events, event{Name: fmt.Sprintf("analog_read_%v", pinNumber), Data: []byte{byte(value >> 24), byte(value >> 16), byte(value >> 8), byte(value & 0xff)}})
+	gobot.Publish(a.board.events[fmt.Sprintf("analog_read_%v", pinNumber)],
+		[]byte{
+			byte(value >> 24),
+			byte(value >> 16),
+			byte(value >> 8),
+			byte(value & 0xff),
+		},
+	)
 	gobot.Expect(t, a.AnalogRead(pinNumber), 133)
+}
+func TestFirmataAdaptorAnalogWrite(t *testing.T) {
+	a := initTestFirmataAdaptor()
+	a.AnalogWrite("1", 50)
 }
 func TestFirmataAdaptorI2cStart(t *testing.T) {
 	a := initTestFirmataAdaptor()
@@ -75,7 +97,7 @@ func TestFirmataAdaptorI2cRead(t *testing.T) {
 	i := []byte{100}
 	i2cReply := map[string][]byte{}
 	i2cReply["data"] = i
-	a.Board.Events = append(a.Board.Events, event{Name: "i2c_reply", I2cReply: i2cReply})
+	gobot.Publish(a.board.events["i2c_reply"], i2cReply)
 	gobot.Expect(t, a.I2cRead(1), i)
 }
 func TestFirmataAdaptorI2cWrite(t *testing.T) {
