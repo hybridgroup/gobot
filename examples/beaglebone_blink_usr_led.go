39d3795f5a5397ffc24c2278a0e8ae45c1a03f4f
Adrian Zankich <adrian@hybridgroup.com>
2014-09-17 15:20:08 -0700

    Add support for blinking usr leds

---
+++ examples/beaglebone_blink_usr_led.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"time"
+
+	"github.com/hybridgroup/gobot"
+	"github.com/hybridgroup/gobot/platforms/beaglebone"
+	"github.com/hybridgroup/gobot/platforms/gpio"
+)
+
+func main() {
+	gbot := gobot.NewGobot()
+
+	beagleboneAdaptor := beaglebone.NewBeagleboneAdaptor("beaglebone")
+	led := gpio.NewLedDriver(beagleboneAdaptor, "led", "usr0")
+
+	work := func() {
+		gobot.Every(1*time.Second, func() {
+			led.Toggle()
+		})
+	}
+
+	robot := gobot.NewRobot("blinkBot",
+		[]gobot.Connection{beagleboneAdaptor},
+		[]gobot.Device{led},
+		work,
+	)
+
+	gbot.AddRobot(robot)
+
+	gbot.Start()
+}
