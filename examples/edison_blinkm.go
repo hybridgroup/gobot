55fd5b1b64be81325e1af0915b2da18763ab4c39
Adrian Zankich <adrian@hybridgroup.com>
2014-09-18 12:45:54 -0700

    Add Edison Support
    Digital write works
    
    Add PWMWrite
    
    Add AnalogRead
    
    Refactor edison adaptor
    
    Enable more digital pins
    
    Enable all digital pins
    
    Add i2c support
    
    Properly close i2c device
    
    Restore proper examples
    
    Add test stub
    
    Add Edison README
    
    Conflicts:
    	scripts/travis.sh

---
+++ examples/edison_blinkm.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/hybridgroup/gobot"
+	"github.com/hybridgroup/gobot/platforms/i2c"
+	"github.com/hybridgroup/gobot/platforms/intel-iot/edison"
+)
+
+func main() {
+	gbot := gobot.NewGobot()
+
+	e := edison.NewEdisonAdaptor("edison")
+	blinkm := i2c.NewBlinkMDriver(e, "blinkm")
+
+	work := func() {
+		gobot.Every(3*time.Second, func() {
+			r := byte(gobot.Rand(255))
+			g := byte(gobot.Rand(255))
+			b := byte(gobot.Rand(255))
+			blinkm.Rgb(r, g, b)
+			fmt.Println("color", blinkm.Color())
+		})
+	}
+
+	robot := gobot.NewRobot("blinkmBot",
+		[]gobot.Connection{e},
+		[]gobot.Device{blinkm},
+		work,
+	)
+
+	gbot.AddRobot(robot)
+	gbot.Start()
+}
