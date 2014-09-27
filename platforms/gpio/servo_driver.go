09bc76d501fa97add9535d059a2501100161c15b
Adrian Zankich <adrian@hybridgroup.com>
2014-09-27 11:34:13 -0700

    Add docs to gpio package

--- platforms/gpio/servo_driver.go
+++ platforms/gpio/servo_driver.go
@@ -4,11 +4,19 @@ import (
 	"github.com/hybridgroup/gobot"
 )
 
+// Represents a Servo
 type ServoDriver struct {
 	gobot.Driver
 	CurrentAngle byte
 }
 
+// NewSerovDriver return a new ServoDriver  given a Servo, name and pin.
+//
+// Adds the following API Commands:
+// 	"Move" - See ServoDriver.Move
+//	"Min" - See ServoDriver.Min
+//	"Center" - See ServoDriver.Center
+//	"Max" - See ServoDriver.Max
 func NewServoDriver(a Servo, name string, pin string) *ServoDriver {
 	s := &ServoDriver{
 		Driver: *gobot.NewDriver(
@@ -46,14 +54,18 @@ func (s *ServoDriver) adaptor() Servo {
 	return s.Adaptor().(Servo)
 }
 
+// Start starts the ServoDriver. Returns true on successful start of the driver.
 func (s *ServoDriver) Start() bool { return true }
-func (s *ServoDriver) Halt() bool  { return true }
-func (s *ServoDriver) Init() bool  { return true }
 
+// Halt halts the ServoDriver. Returns true on successful halt of the driver.
+func (s *ServoDriver) Halt() bool { return true }
+
+// InitServo initializes the ServoDriver on platforms which require an explicit initialization.
 func (s *ServoDriver) InitServo() {
 	s.adaptor().InitServo()
 }
 
+// Move sets the servo to the specified angle
 func (s *ServoDriver) Move(angle uint8) {
 	if !(angle >= 0 && angle <= 180) {
 		panic("Servo angle must be an integer between 0-180")
@@ -62,14 +74,17 @@ func (s *ServoDriver) Move(angle uint8) {
 	s.adaptor().ServoWrite(s.Pin(), s.angleToSpan(angle))
 }
 
+// Min sets the servo to it's minimum position
 func (s *ServoDriver) Min() {
 	s.Move(0)
 }
 
+// Center sets the servo to it's center position
 func (s *ServoDriver) Center() {
 	s.Move(90)
 }
 
+// Max sets the servo to its maximum position
 func (s *ServoDriver) Max() {
 	s.Move(180)
 }
