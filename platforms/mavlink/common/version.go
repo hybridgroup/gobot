8684b7d5a1170159746347d885ab3d11ec57a2b8
Adrian Zankich <adrian@hybridgroup.com>
2014-09-26 19:30:52 -0700

    Add mavlink docs

--- platforms/mavlink/common/version.go
+++ platforms/mavlink/common/version.go
@@ -1,11 +1,13 @@
 package mavlink
 
-/** @file
- *	@brief MAVLink comm protocol built from common.xml
- *	@see http://pixhawk.ethz.ch/software/mavlink
- */
+//
+// MAVLink comm protocol built from common.xml
+// http://pixhawk.ethz.ch/software/mavlink
+//
 
-const MAVLINK_BUILD_DATE = "Thu Jul 31 22:19:34 2014"
-const MAVLINK_WIRE_PROTOCOL_VERSION = "1.0"
-const MAVLINK_MAX_DIALECT_PAYLOAD_SIZE = 255
-const MAVLINK_VERSION = 3
+const (
+	MAVLINK_BUILD_DATE               = "Fri Sep 26 19:23:02 2014"
+	MAVLINK_WIRE_PROTOCOL_VERSION    = "1.0"
+	MAVLINK_MAX_DIALECT_PAYLOAD_SIZE = 255
+	MAVLINK_VERSION                  = 3
+)
