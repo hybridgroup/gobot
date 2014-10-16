e482b6d3b54be1694da73f60805d4cd162c78a7c
Javier Cervantes <1.27201@gmail.com>
2014-10-16 16:26:35 -0500

    Adding godoc documentation to pebble package

--- platforms/pebble/pebble_adaptor.go
+++ platforms/pebble/pebble_adaptor.go
@@ -8,6 +8,7 @@ type PebbleAdaptor struct {
 	gobot.Adaptor
 }
 
+// NewPebbleAdaptor creates a new pebble adaptor with specified name
 func NewPebbleAdaptor(name string) *PebbleAdaptor {
 	return &PebbleAdaptor{
 		Adaptor: *gobot.NewAdaptor(
@@ -17,18 +18,22 @@ func NewPebbleAdaptor(name string) *PebbleAdaptor {
 	}
 }
 
+// Connect returns true if connection to pebble is established succesfully
 func (a *PebbleAdaptor) Connect() bool {
 	return true
 }
 
+// Reconnect retries connection to pebble. Returns true if succesfull
 func (a *PebbleAdaptor) Reconnect() bool {
 	return true
 }
 
+// Disconnect returns true if connection to pebble is closed succesfully
 func (a *PebbleAdaptor) Disconnect() bool {
 	return true
 }
 
+// Finalize returns true if connection to pebble is finalized succesfully
 func (a *PebbleAdaptor) Finalize() bool {
 	return true
 }
--- platforms/pebble/pebble_driver.go
+++ platforms/pebble/pebble_driver.go
@@ -9,9 +9,15 @@ type PebbleDriver struct {
 	Messages []string
 }
 
-type PebbleInterface interface {
-}
-
+// NewPebbleDriver creates a new pebble driver with specified name
+// Adds following events:
+//		button - Sent when a pebble button is pressed
+//		accel - Pebble watch acceleromenter data
+//		tab - When a pebble watch tap event is detected
+// And the following API commands:
+//		"publish_event"
+//		"send_notification"
+//		"pending_message"
 func NewPebbleDriver(adaptor *PebbleAdaptor, name string) *PebbleDriver {
 	p := &PebbleDriver{
 		Driver: *gobot.NewDriver(
@@ -31,7 +37,7 @@ func NewPebbleDriver(adaptor *PebbleAdaptor, name string) *PebbleDriver {
 		return nil
 	})
 
-	p.AddCommand("SendNotification", func(params map[string]interface{}) interface{} {
+	p.AddCommand("send_notification", func(params map[string]interface{}) interface{} {
 		p.SendNotification(params["message"].(string))
 		return nil
 	})
@@ -42,23 +48,31 @@ func NewPebbleDriver(adaptor *PebbleAdaptor, name string) *PebbleDriver {
 
 	return p
 }
+
+// adaptor returns asociated adaptor
 func (d *PebbleDriver) adaptor() *PebbleAdaptor {
 	return d.Adaptor().(*PebbleAdaptor)
 }
 
+// Start returns true if driver is initialized correctly
 func (d *PebbleDriver) Start() bool { return true }
 
+// Halt returns true if driver is halted succesfully
 func (d *PebbleDriver) Halt() bool { return true }
 
+// PublishEvent publishes event with specified name and data in gobot
 func (d *PebbleDriver) PublishEvent(name string, data string) {
 	gobot.Publish(d.Event(name), data)
 }
 
+// SendNotification appends message to list of notifications to be sent to watch
 func (d *PebbleDriver) SendNotification(message string) string {
 	d.Messages = append(d.Messages, message)
 	return message
 }
 
+// PendingMessages returns messages to be sent as notifications to pebble
+// (Not intented to be used directly)
 func (d *PebbleDriver) PendingMessage() string {
 	if len(d.Messages) < 1 {
 		return ""
