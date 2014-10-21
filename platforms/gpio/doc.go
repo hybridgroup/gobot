3dce190b27acd68ae7fa9482fae60fb25f8ea823
Javier Cervantes <1.27201@gmail.com>
2014-10-21 13:36:42 -0500

    Adding doc.go to gpio package

---
+++ platforms/gpio/doc.go
@@ -0,0 +1,11 @@
+/*
+This package provides drivers for General Purpose Input/Output (GPIO) (https://en.wikipedia.org/wiki/General_Purpose_Input/Output) devices . It is normally not used directly, but instead is registered by an adaptor such as firmata (https://github.com/hybridgroup/gobot/platforms/firmata) that supports the needed interfaces for GPIO devices.
+
+Installing:
+
+	go get github.com/hybridgroup/gobot && go install github.com/hybridgroup/gobot/platforms/gpio
+
+For further information refer to gpio README:
+https://github.com/hybridgroup/gobot/blob/master/platforms/gpio/README.md
+*/
+package gpio
