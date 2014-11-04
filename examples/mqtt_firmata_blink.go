ccbe8b21f5b050ebd01f0402b4c37c4d8620dc57
deadprogram <ron@hybridgroup.com>
2014-11-04 08:54:06 -0800

    Working mqtt examples

--- examples/mqtt_firmata_blink.go
+++ examples/mqtt_firmata_blink.go
@@ -1,35 +1,43 @@
 package main
 
 import (
-  "github.com/hybridgroup/gobot"
-  "github.com/hybridgroup/gobot/platforms/mqtt"
-  "github.com/hybridgroup/gobot/platforms/firmata"
-  "github.com/hybridgroup/gobot/platforms/gpio"
+	"github.com/hybridgroup/gobot"
+	"github.com/hybridgroup/gobot/platforms/firmata"
+	"github.com/hybridgroup/gobot/platforms/gpio"
+	"github.com/hybridgroup/gobot/platforms/mqtt"
+	"time"
 )
 
 func main() {
-  gbot := gobot.NewGobot()
+	gbot := gobot.NewGobot()
 
-  mqttAdaptor := mqtt.NewMqttAdaptor("server", "localhost:1883")
-  firmataAdaptor := firmata.NewFirmataAdaptor("arduino", "/dev/ttyACM0")
-  led := gpio.NewLedDriver(firmataAdaptor, "led", "13")
+	mqttAdaptor := mqtt.NewMqttAdaptor("server", "tcp://localhost:1883")
+	firmataAdaptor := firmata.NewFirmataAdaptor("arduino", "/dev/ttyACM0")
+	led := gpio.NewLedDriver(firmataAdaptor, "led", "13")
 
-  work := func() {
-    mqttAdaptor.On('lights/on', func(data interface{}) {
-      led.On()
-    })
-    mqttAdaptor.On('lights/off', func(data interface{}) {
-      led.Off()
-    })
-  }
+	work := func() {
+		mqttAdaptor.On("lights/on", func(data interface{}) {
+			led.On()
+		})
+		mqttAdaptor.On("lights/off", func(data interface{}) {
+			led.Off()
+		})
+		data := []byte("")
+		gobot.Every(1*time.Second, func() {
+			mqttAdaptor.Publish("lights/on", data)
+		})
+		gobot.Every(2*time.Second, func() {
+			mqttAdaptor.Publish("lights/off", data)
+		})
+	}
 
-  robot := gobot.NewRobot("mqttBot",
-    []gobot.Connection{mqttAdaptor, firmataAdaptor},
-    []gobot.Device{led},
-    work,
-  )
+	robot := gobot.NewRobot("mqttBot",
+		[]gobot.Connection{mqttAdaptor, firmataAdaptor},
+		[]gobot.Device{led},
+		work,
+	)
 
-  gbot.AddRobot(robot)
+	gbot.AddRobot(robot)
 
-  gbot.Start()
+	gbot.Start()
 }
