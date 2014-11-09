aad77ff5ab4d7b73f57052667a0ff6c6fd88c94d
Adrian Zankich <adrian@hybridgroup.com>
2014-11-09 14:35:36 -0800

    Refactor beaglebone tests

--- platforms/beaglebone/beaglebone_adaptor_test.go
+++ platforms/beaglebone/beaglebone_adaptor_test.go
@@ -1,29 +1,28 @@
 package beaglebone
 
 import (
+	"strings"
 	"testing"
 
 	"github.com/hybridgroup/gobot"
 	"github.com/hybridgroup/gobot/sysfs"
 )
 
-func initTestBeagleboneAdaptor() *BeagleboneAdaptor {
-	a := NewBeagleboneAdaptor("myAdaptor")
-	a.connect = func() {}
-	a.Connect()
-	a.DigitalWrite("P9_12", 1)
-	a.i2cDevice = new(gobot.NullReadWriteCloser)
-	return a
-}
-
-func TestBeagleboneAdaptorFinalize(t *testing.T) {
-	gobot.Assert(t, initTestBeagleboneAdaptor().Finalize(), true)
-}
-
-func TestBeagleboneAdaptorDigitalIO(t *testing.T) {
-	a := initTestBeagleboneAdaptor()
-
+func TestBeagleboneAdaptor(t *testing.T) {
+	glob = func(pattern string) (matches []string, err error) {
+		return make([]string, 2), nil
+	}
 	fs := sysfs.NewMockFilesystem([]string{
+		"/dev/i2c-1",
+		"/sys/devices/bone_capemgr.4",
+		"/sys/devices/ocp.3",
+		"/sys/devices/ocp.3/gpio-leds.8/leds/beaglebone:green:usr1/brightness",
+		"/sys/devices/ocp.3/helper.5",
+		"/sys/devices/ocp.3/helper.5/AIN1",
+		"/sys/devices/ocp.3/pwm_test_P9_14.5",
+		"/sys/devices/ocp.3/pwm_test_P9_14.5/run",
+		"/sys/devices/ocp.3/pwm_test_P9_14.5/period",
+		"/sys/devices/ocp.3/pwm_test_P9_14.5/duty",
 		"/sys/class/gpio/export",
 		"/sys/class/gpio/unexport",
 		"/sys/class/gpio/gpio60/value",
@@ -33,23 +32,80 @@ func TestBeagleboneAdaptorDigitalIO(t *testing.T) {
 	})
 
 	sysfs.SetFilesystem(fs)
+	a := NewBeagleboneAdaptor("myAdaptor")
+	a.slots = "/sys/devices/bone_capemgr.4"
+	a.ocp = "/sys/devices/ocp.3"
+	a.helper = "/sys/devices/ocp.3/helper.5"
+
+	a.Connect()
+
+	// PWM
+	glob = func(pattern string) (matches []string, err error) {
+		pattern = strings.TrimSuffix(pattern, "*")
+		return []string{pattern + "5"}, nil
+	}
+
+	a.PwmWrite("P9_14", 175)
+	gobot.Assert(
+		t,
+		fs.Files["/sys/devices/ocp.3/pwm_test_P9_14.5/period"].Contents,
+		"500000",
+	)
+	gobot.Assert(
+		t,
+		fs.Files["/sys/devices/ocp.3/pwm_test_P9_14.5/duty"].Contents,
+		"156862",
+	)
+
+	a.AnalogWrite("P9_14", 175)
+	gobot.Assert(
+		t,
+		fs.Files["/sys/devices/ocp.3/pwm_test_P9_14.5/period"].Contents,
+		"500000",
+	)
+	gobot.Assert(
+		t,
+		fs.Files["/sys/devices/ocp.3/pwm_test_P9_14.5/duty"].Contents,
+		"156862",
+	)
+
+	a.ServoWrite("P9_14", 100)
+	gobot.Assert(
+		t,
+		fs.Files["/sys/devices/ocp.3/pwm_test_P9_14.5/period"].Contents,
+		"20000000",
+	)
+	gobot.Assert(
+		t,
+		fs.Files["/sys/devices/ocp.3/pwm_test_P9_14.5/duty"].Contents,
+		"17222222",
+	)
+
+	// Analog
+	fs.Files["/sys/devices/ocp.3/helper.5/AIN1"].Contents = "567\n"
+	i := a.AnalogRead("P9_40")
+	gobot.Assert(t, i, 567)
+
+	// DigitalIO
+	a.DigitalWrite("usr1", 1)
+	gobot.Assert(t,
+		fs.Files["/sys/devices/ocp.3/gpio-leds.8/leds/beaglebone:green:usr1/brightness"].Contents,
+		"1",
+	)
+
 	a.DigitalWrite("P9_12", 1)
 	gobot.Assert(t, fs.Files["/sys/class/gpio/gpio60/value"].Contents, "1")
 
 	fs.Files["/sys/class/gpio/gpio10/value"].Contents = "1"
-	i := a.DigitalRead("P8_31")
+	i = a.DigitalRead("P8_31")
 	gobot.Assert(t, i, 1)
-}
 
-func TestBeagleboneAdaptorI2c(t *testing.T) {
-	a := initTestBeagleboneAdaptor()
-	fs := sysfs.NewMockFilesystem([]string{
-		"/dev/i2c-1",
-	})
-	sysfs.SetFilesystem(fs)
+	// I2c
 	sysfs.SetSyscall(&sysfs.MockSyscall{})
 	a.I2cStart(0xff)
 
 	a.I2cWrite([]byte{0x00, 0x01})
 	gobot.Assert(t, a.I2cRead(2), []byte{0x00, 0x01})
+
+	gobot.Assert(t, a.Finalize(), true)
 }
--- platforms/beaglebone/pwm_pin.go
+++ platforms/beaglebone/pwm_pin.go
@@ -2,8 +2,8 @@ package beaglebone
 
 import (
 	"fmt"
+	"github.com/hybridgroup/gobot/sysfs"
 	"os"
-	"path/filepath"
 	"strings"
 )
 
@@ -13,22 +13,14 @@ type pwmPin struct {
 }
 
 // newPwmPin creates a new pwm pin with specified pin number
-func newPwmPin(pinNum string) *pwmPin {
-	var err error
-	var fi *os.File
+func newPwmPin(pinNum string, ocp string) *pwmPin {
+	var fi sysfs.File
 
 	d := &pwmPin{
 		pinNum: strings.ToUpper(pinNum),
 	}
 
-	ensureSlot(fmt.Sprintf("bone_pwm_%v", d.pinNum))
-
-	ocp, err := filepath.Glob(Ocp)
-	if err != nil {
-		panic(err)
-	}
-
-	pwmDevice, err := filepath.Glob(fmt.Sprintf("%v/pwm_test_%v.*", ocp[0], d.pinNum))
+	pwmDevice, err := glob(fmt.Sprintf("%v/pwm_test_%v.*", ocp, d.pinNum))
 	if err != nil {
 		panic(err)
 	}
@@ -36,7 +28,8 @@ func newPwmPin(pinNum string) *pwmPin {
 	d.pwmDevice = pwmDevice[0]
 
 	for i := 0; i < 10; i++ {
-		fi, err = os.OpenFile(fmt.Sprintf("%v/run", d.pwmDevice), os.O_RDWR|os.O_APPEND, 0666)
+		fi, err = sysfs.OpenFile(fmt.Sprintf("%v/run", d.pwmDevice), os.O_RDWR|os.O_APPEND, 0666)
+		defer fi.Close()
 		if err != nil && i == 9 {
 			panic(err)
 		} else {
@@ -46,15 +39,14 @@ func newPwmPin(pinNum string) *pwmPin {
 
 	fi.WriteString("1")
 	fi.Sync()
-	fi.Close()
 
 	for {
-		if _, err := os.Stat(fmt.Sprintf("%v/period", d.pwmDevice)); err == nil {
+		if _, err := sysfs.OpenFile(fmt.Sprintf("%v/period", d.pwmDevice), os.O_RDONLY, 0644); err == nil {
 			break
 		}
 	}
 	for {
-		if _, err := os.Stat(fmt.Sprintf("%v/duty", d.pwmDevice)); err == nil {
+		if _, err := sysfs.OpenFile(fmt.Sprintf("%v/duty", d.pwmDevice), os.O_RDONLY, 0644); err == nil {
 			break
 		}
 	}
@@ -63,30 +55,27 @@ func newPwmPin(pinNum string) *pwmPin {
 
 // pwmWrite writes to a pwm pin with specified period and duty
 func (p *pwmPin) pwmWrite(period string, duty string) {
-	var err error
-	var fi *os.File
-
-	fi, err = os.OpenFile(fmt.Sprintf("%v/period", p.pwmDevice), os.O_WRONLY|os.O_APPEND, 0666)
+	f1, err := sysfs.OpenFile(fmt.Sprintf("%v/period", p.pwmDevice), os.O_WRONLY|os.O_APPEND, 0666)
+	defer f1.Close()
 	if err != nil {
 		panic(err)
 	}
-	fi.WriteString(period)
-	fi.Close()
+	f1.WriteString(period)
 
-	fi, err = os.OpenFile(fmt.Sprintf("%v/duty", p.pwmDevice), os.O_WRONLY|os.O_APPEND, 0666)
+	f2, err := sysfs.OpenFile(fmt.Sprintf("%v/duty", p.pwmDevice), os.O_WRONLY|os.O_APPEND, 0666)
+	defer f2.Close()
 	if err != nil {
 		panic(err)
 	}
-	fi.WriteString(duty)
-	fi.Close()
+	f2.WriteString(duty)
 }
 
 // releae writes string to close a pwm pin
 func (p *pwmPin) release() {
-	fi, err := os.OpenFile(fmt.Sprintf("%v/run", p.pwmDevice), os.O_WRONLY|os.O_APPEND, 0666)
+	fi, err := sysfs.OpenFile(fmt.Sprintf("%v/run", p.pwmDevice), os.O_WRONLY|os.O_APPEND, 0666)
+	defer fi.Close()
 	if err != nil {
 		panic(err)
 	}
 	fi.WriteString("0")
-	fi.Close()
 }
