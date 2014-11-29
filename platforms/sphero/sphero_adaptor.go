61e70f7571ed4bde14c33de348d12ee039e35f05
Adrian Zankich <adrian@hybridgroup.com>
2014-11-28 18:21:02 -0800

    Refactor sphero to use new driver and adaptor interfaces

--- platforms/sphero/sphero_adaptor.go
+++ platforms/sphero/sphero_adaptor.go
@@ -7,41 +7,42 @@ import (
 	"github.com/tarm/goserial"
 )
 
-var _ gobot.AdaptorInterface = (*SpheroAdaptor)(nil)
+var _ gobot.Adaptor = (*SpheroAdaptor)(nil)
 
 // Represents a Connection to a Sphero
 type SpheroAdaptor struct {
-	gobot.Adaptor
-	sp      io.ReadWriteCloser
-	connect func(*SpheroAdaptor) (err error)
+	name      string
+	port      string
+	sp        io.ReadWriteCloser
+	connected bool
+	connect   func(*SpheroAdaptor) (err error)
 }
 
 // NewSpheroAdaptor returns a new SpheroAdaptor given a name and port
 func NewSpheroAdaptor(name string, port string) *SpheroAdaptor {
 	return &SpheroAdaptor{
-		Adaptor: *gobot.NewAdaptor(
-			name,
-			"SpheroAdaptor",
-			port,
-		),
+		name: name,
+		port: port,
 		connect: func(a *SpheroAdaptor) (err error) {
 			c := &serial.Config{Name: a.Port(), Baud: 115200}
 			s, err := serial.OpenPort(c)
 			if err != nil {
 				return err
 			}
+			a.connected = true
 			a.sp = s
 			return
 		},
 	}
 }
+func (a *SpheroAdaptor) Name() string { return a.name }
+func (a *SpheroAdaptor) Port() string { return a.port }
 
 // Connect initiates a connection to the Sphero. Returns true on successful connection.
 func (a *SpheroAdaptor) Connect() (errs []error) {
 	if err := a.connect(a); err != nil {
 		return []error{err}
 	}
-	a.SetConnected(true)
 	return
 }
 
@@ -49,7 +50,7 @@ func (a *SpheroAdaptor) Connect() (errs []error) {
 // it will first close that connection and then establish a new connection.
 // Returns true on Successful reconnection
 func (a *SpheroAdaptor) Reconnect() (errs []error) {
-	if a.Connected() == true {
+	if a.connected == true {
 		a.Disconnect()
 	}
 	return a.Connect()
@@ -60,11 +61,11 @@ func (a *SpheroAdaptor) Disconnect() (errs []error) {
 	if err := a.sp.Close(); err != nil {
 		return []error{err}
 	}
-	a.SetConnected(false)
+	a.connected = false
 	return
 }
 
 // Finalize finalizes the SpheroAdaptor
 func (a *SpheroAdaptor) Finalize() (errs []error) {
-	return
+	return a.Disconnect()
 }
