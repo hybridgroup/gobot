22b84cc6ef3cb174aa552f4def5cd1c2253356a4
Adrian Zankich <adrian@hybridgroup.com>
2014-12-31 06:12:25 -0800

    go lint and documentation tweaks for the sysfs package

--- sysfs/i2c_device.go
+++ sysfs/i2c_device.go
@@ -1,16 +1,17 @@
 package sysfs
 
 import (
-	"errors"
 	"fmt"
 	"io"
 	"os"
 	"syscall"
 )
 
+// I2CSlave is the linux default ioctrl request code
 const I2CSlave = 0x0703
 
-// NewI2cDevice creates a new io.ReadWriteCloser with the proper ioctrl given an i2c bus location and device address
+// NewI2cDevice returns an io.ReadWriteCloser with the proper ioctrl given
+// an i2c bus location and device address
 func NewI2cDevice(location string, address byte) (io.ReadWriteCloser, error) {
 	file, err := OpenFile(location, os.O_RDWR, os.ModeExclusive)
 
@@ -26,7 +27,7 @@ func NewI2cDevice(location string, address byte) (io.ReadWriteCloser, error) {
 	)
 
 	if errno != 0 {
-		return nil, errors.New(fmt.Sprintf("Failed with syscall.Errno %v", errno))
+		return nil, fmt.Errorf("Failed with syscall.Errno %v", errno)
 	}
 
 	return file, nil
