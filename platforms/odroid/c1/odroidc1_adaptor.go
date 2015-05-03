6a688e0f16161fc74165c76ff95c6bf32ddc792b
Dean Hart <dhart@alldigital.com>
2015-05-03 10:44:13 -0700

    Added i2c, pwm and analog support for ODroid C1

--- platforms/odroid/c1/odroidc1_adaptor.go
+++ platforms/odroid/c1/odroidc1_adaptor.go
@@ -2,11 +2,11 @@ package c1
 
 import (
 	"errors"
+	"fmt"
 	"io"
-	"log"
-	//"io/ioutil"
-	//"strconv"
-	//"strings"
+	"os"
+	"strconv"
+	"strings"
 
 	"github.com/hybridgroup/gobot"
 	"github.com/hybridgroup/gobot/platforms/gpio"
@@ -14,18 +14,25 @@ import (
 	"github.com/hybridgroup/gobot/sysfs"
 )
 
+const pwmBase string = "/sys/devices/platform/pwm-ctrl"
+const analogBase string = "/sys/class/saradc"
+const i2cBase string = "/sys/bus/i2c"
+
 var _ gobot.Adaptor = (*ODroidC1Adaptor)(nil)
 
 var _ gpio.DigitalReader = (*ODroidC1Adaptor)(nil)
 var _ gpio.DigitalWriter = (*ODroidC1Adaptor)(nil)
+var _ gpio.AnalogReader = (*ODroidC1Adaptor)(nil)
+var _ gpio.PwmWriter = (*ODroidC1Adaptor)(nil)
+var _ gpio.ServoWriter = (*ODroidC1Adaptor)(nil)
 
 var _ i2c.I2c = (*ODroidC1Adaptor)(nil)
 
 type ODroidC1Adaptor struct {
 	name        string
-	//revision    string
 	i2cLocation string
 	digitalPins map[int]sysfs.DigitalPin
+	pwmPins     map[int]*pwmPin
 	i2cDevice   io.ReadWriteCloser
 }
 
@@ -41,7 +48,6 @@ var pins = map[string]int{
 	"15": 115,
 	"16": 104,
 	"18": 102,
-	"19": 107,
 	"21": 106,
 	"22": 103,
 	"23": 105,
@@ -52,41 +58,62 @@ var pins = map[string]int{
 	"29": 101,
 	"31": 100,
 	"32": 99,
-	"33": 108,
 	"35": 97,
 	"36": 98,
 }
 
-// NewODroidC1Adaptor creates an ODroidC1Adaptor with specified name
-func NewODroidC1Adaptor(name string) *ODroidC1Adaptor {
-	r := &ODroidC1Adaptor{
-		name:        name,
-		digitalPins: make(map[int]sysfs.DigitalPin),
-		i2cLocation: "/sys/bus/i2c",
-	}
-
-	return r
+var pwmPins = map[string]map[int]int{ 
+	"19": map[int]int{
+		107: 1,
+	},
+	"33": map[int]int{
+		108: 0,
+	},
 }
 
-func (r *ODroidC1Adaptor) Name() string { return r.name }
+var analogPins = map[string]string{
+	"37": "saradc_ch1",
+	"40": "saradc_ch0",
+}
+
+// NewODroidC1Adaptor creates an ODroidC1Adaptor with specified name
+func NewODroidC1Adaptor(name string) *ODroidC1Adaptor {
+	o := &ODroidC1Adaptor{
+		name:        name,
+		digitalPins: make(map[int]sysfs.DigitalPin),
+		pwmPins:     make(map[int]*pwmPin),
+		i2cLocation: i2cBase,
+	}
+
+	return o
+}
+
+func (o *ODroidC1Adaptor) Name() string { return o.name }
 
 // Connect starts conection with board and creates
 // digitalPins and pwmPins adaptor maps
-func (r *ODroidC1Adaptor) Connect() (errs []error) {
+func (o *ODroidC1Adaptor) Connect() (errs []error) {
 	return
 }
 
 // Finalize closes connection to board and pins
-func (r *ODroidC1Adaptor) Finalize() (errs []error) {
-	for _, pin := range r.digitalPins {
+func (o *ODroidC1Adaptor) Finalize() (errs []error) {
+	for _, pin := range o.digitalPins {
 		if pin != nil {
 			if err := pin.Unexport(); err != nil {
 				errs = append(errs, err)
 			}
 		}
 	}
-	if r.i2cDevice != nil {
-		if err := r.i2cDevice.Close(); err != nil {
+	for _, pin := range o.pwmPins {
+		if pin != nil {
+			if err := pin.release(); err != nil {
+				errs = append(errs, err)
+			}
+		}
+	}
+	if o.i2cDevice != nil {
+		if err := o.i2cDevice.Close(); err != nil {
 			errs = append(errs, err)
 		}
 	}
@@ -94,9 +121,7 @@ func (r *ODroidC1Adaptor) Finalize() (errs []error) {
 }
 
 // digitalPin returns matched digitalPin for specified values
-func (r *ODroidC1Adaptor) digitalPin(pin string, dir string) (sysfsPin sysfs.DigitalPin, err error) {
-	log.Println("Looking for digitalPin " + pin + " in dir " + dir)
-
+func (o *ODroidC1Adaptor) digitalPin(pin string, dir string) (sysfsPin sysfs.DigitalPin, err error) {
 	var i int
 
 	if val, ok := pins[pin]; ok {
@@ -106,28 +131,23 @@ func (r *ODroidC1Adaptor) digitalPin(pin string, dir string) (sysfsPin sysfs.Dig
 		return
 	}
 
-	if r.digitalPins[i] == nil {
-		r.digitalPins[i] = sysfs.NewDigitalPin(i)
-		if err = r.digitalPins[i].Export(); err != nil {
-			log.Println("Err with Export")
-			log.Fatal(err)
+	if o.digitalPins[i] == nil {
+		o.digitalPins[i] = sysfs.NewDigitalPin(i)
+		if err = o.digitalPins[i].Export(); err != nil {
 			return
 		}
 	}
 
-	if err = r.digitalPins[i].Direction(dir); err != nil {
-			log.Println("Err with Direction")
-			log.Fatal(err)
+	if err = o.digitalPins[i].Direction(dir); err != nil {
 		return
 	}
 
-	//log.Println("Returning pin: " + r.digitalPins[i].pin + ", label: " + r.digitalPins[i].label)
-	return r.digitalPins[i], nil
+	return o.digitalPins[i], nil
 }
 
 // DigitalRead reads digital value from pin
-func (r *ODroidC1Adaptor) DigitalRead(pin string) (val int, err error) {
-	sysfsPin, err := r.digitalPin(pin, sysfs.IN)
+func (o *ODroidC1Adaptor) DigitalRead(pin string) (val int, err error) {
+	sysfsPin, err := o.digitalPin(pin, sysfs.IN)
 	if err != nil {
 		return
 	}
@@ -135,10 +155,8 @@ func (r *ODroidC1Adaptor) DigitalRead(pin string) (val int, err error) {
 }
 
 // DigitalWrite writes digital value to specified pin
-func (r *ODroidC1Adaptor) DigitalWrite(pin string, val byte) (err error) {
-	log.Println("DigitalWrite " + pin )
-
-	sysfsPin, err := r.digitalPin(pin, sysfs.OUT)
+func (o *ODroidC1Adaptor) DigitalWrite(pin string, val byte) (err error) {
+	sysfsPin, err := o.digitalPin(pin, sysfs.OUT)
 	if err != nil {
 		return err
 	}
@@ -146,20 +164,109 @@ func (r *ODroidC1Adaptor) DigitalWrite(pin string, val byte) (err error) {
 }
 
 // I2cStart starts a i2c device in specified address
-func (r *ODroidC1Adaptor) I2cStart(address byte) (err error) {
-	r.i2cDevice, err = sysfs.NewI2cDevice(r.i2cLocation, address)
+func (o *ODroidC1Adaptor) I2cStart(address byte) (err error) {
+	o.i2cDevice, err = sysfs.NewI2cDevice(o.i2cLocation, address)
 	return err
 }
 
 // I2CWrite writes data to i2c device
-func (r *ODroidC1Adaptor) I2cWrite(data []byte) (err error) {
-	_, err = r.i2cDevice.Write(data)
+func (o *ODroidC1Adaptor) I2cWrite(data []byte) (err error) {
+	_, err = o.i2cDevice.Write(data)
 	return
 }
 
 // I2cRead returns value from i2c device using specified size
-func (r *ODroidC1Adaptor) I2cRead(size uint) (data []byte, err error) {
+func (o *ODroidC1Adaptor) I2cRead(size uint) (data []byte, err error) {
 	data = make([]byte, size)
-	_, err = r.i2cDevice.Read(data)
+	_, err = o.i2cDevice.Read(data)
+	return
+}
+
+// translatePwmPin converts pwm pin name to pin position
+func (o *ODroidC1Adaptor) translatePwmPin(pin string) (gpioNum int, pwmNum int, err error) {
+	pwm := pwmPins[pin]
+	if pwm == nil {
+		err = errors.New("Not a valid pwm pin")
+		return
+	}
+	
+	return pwm[0], pwm[1], nil
+	
+}
+
+// pwmPin retrieves pwm pin value by name
+func (o *ODroidC1Adaptor) pwmPin(pin string) (gpioNum int, pwmNum int, err error) {
+	gpioNum, pwmNum, err = o.translatePwmPin(pin)
+	if err != nil {
+		return
+	}
+
+	if o.pwmPins[gpioNum] == nil {
+		o.pwmPins[gpioNum], err = newPwmPin(pin, gpioNum, pwmNum, pwmBase)
+		if err != nil {
+			return
+		}
+	}
+	return
+}
+
+// pwmWrite writes pwm value to specified pin
+func (o *ODroidC1Adaptor) pwmWrite(pin string, val byte) (err error) {
+	gpioNum, _, err := o.pwmPin(pin)
+	if err != nil {
+		return
+	}
+	freq := 500000.0
+	duty := gobot.FromScale(float64(val), 0, 255.0)
+	return o.pwmPins[gpioNum].pwmWrite(strconv.Itoa(int(freq)), strconv.Itoa(int(freq*duty)))
+}
+
+// PwmWrite writes the 0-254 value to the specified pin
+func (o *ODroidC1Adaptor) PwmWrite(pin string, val byte) (err error) {
+	return o.pwmWrite(pin, val)
+}
+
+// ServoWrite writes the 0-180 degree val to the specified pin.
+func (o *ODroidC1Adaptor) ServoWrite(pin string, val byte) (err error) {
+	gpioNum, _, err := o.pwmPin(pin)
+	if err != nil {
+		return err
+	}
+	freq := 16666666.0
+	duty := (gobot.FromScale(float64(val), 0, 180.0) * 0.115) + 0.05
+	return o.pwmPins[gpioNum].pwmWrite(strconv.Itoa(int(freq)), strconv.Itoa(int(freq*duty)))
+}
+
+// translateAnalogPin converts analog pin name to pin position
+func (o *ODroidC1Adaptor) translateAnalogPin(pin string) (value string, err error) {
+	for key, value := range analogPins {
+		if key == pin {
+			return value, nil
+		}
+	}
+	err = errors.New("Not a valid pin")
+	return
+}
+
+// AnalogRead returns an analog value from specified pin
+func (o *ODroidC1Adaptor) AnalogRead(pin string) (val int, err error) {
+	analogPin, err := o.translateAnalogPin(pin)
+	if err != nil {
+		return
+	}
+	fi, err := sysfs.OpenFile(fmt.Sprintf("%v/%v", analogBase, analogPin), os.O_RDONLY, 0644)
+	defer fi.Close()
+
+	if err != nil {
+		return
+	}
+
+	var buf = make([]byte, 1024)
+	_, err = fi.Read(buf)
+	if err != nil {
+		return
+	}
+
+	val, _ = strconv.Atoi(strings.Split(string(buf), "\n")[0])
 	return
 }
--- platforms/odroid/c1/odroidc1_adaptor_test.go
+++ platforms/odroid/c1/odroidc1_adaptor_test.go
@@ -1,6 +1,7 @@
 package c1
 
 import (
+	"errors"
 	"testing"
 
 	"github.com/hybridgroup/gobot"
@@ -36,6 +37,9 @@ func TestODroidC1AdaptorFinalize(t *testing.T) {
 	fs := sysfs.NewMockFilesystem([]string{
 		"/sys/class/gpio/export",
 		"/sys/class/gpio/unexport",
+		
+		"/sys/class/gpio/gpio74/value",
+		"/sys/class/gpio/gpio74/direction",
 	})
 
 	sysfs.SetFilesystem(fs)
@@ -46,7 +50,7 @@ func TestODroidC1AdaptorFinalize(t *testing.T) {
 
 func TestODroidC1AdaptorDigitalIO(t *testing.T) {
 	a := initTestODroidC1Adaptor()
-	fs := sysfs.NewMockFilesystem([]string{
+	fs := sysfs.NewMockFilesystem([]string{		
 		"/sys/class/gpio/export",
 		"/sys/class/gpio/unexport",
 		"/sys/class/gpio/gpio83/value",
@@ -63,6 +67,65 @@ func TestODroidC1AdaptorDigitalIO(t *testing.T) {
 	a.DigitalWrite("13", 1)
 	i, _ := a.DigitalRead("13")
 	gobot.Assert(t, i, 1)
+}
+
+func TestODroidC1AdaptorPWM(t *testing.T) {
+	a := initTestODroidC1Adaptor()
+	fs := sysfs.NewMockFilesystem([]string{
+		"/sys/devices/platform/pwm-ctrl/duty0",
+		"/sys/devices/platform/pwm-ctrl/freq0",
+		"/sys/devices/platform/pwm-ctrl/enable0",
+	})
+
+	sysfs.SetFilesystem(fs)
+
+	// PWM
+	a.PwmWrite("33", 175)
+	gobot.Assert(
+		t,
+		fs.Files["/sys/devices/platform/pwm-ctrl/freq0"].Contents,
+		"500000",
+	)
+	gobot.Assert(
+		t,
+		fs.Files["/sys/devices/platform/pwm-ctrl/duty0"].Contents,
+		"343137",
+	)
+
+	a.ServoWrite("33", 100)
+	gobot.Assert(
+		t,
+		fs.Files["/sys/devices/platform/pwm-ctrl/freq0"].Contents,
+		"16666666",
+	)
+	gobot.Assert(
+		t,
+		fs.Files["/sys/devices/platform/pwm-ctrl/duty0"].Contents,
+		"1898148",
+	)
+}
+
+func TestODroidC1AdaptorAnalog(t *testing.T) {
+	a := initTestODroidC1Adaptor()
+	fs := sysfs.NewMockFilesystem([]string{
+		"/sys/class/saradc/saradc_ch0",
+		"/sys/class/saradc/saradc_ch1",
+	})
+
+	sysfs.SetFilesystem(fs)
+
+	// Analog
+	fs.Files["/sys/class/saradc/saradc_ch0"].Contents = "567\n"
+	i, _ := a.AnalogRead("40")
+	gobot.Assert(t, i, 567)
+	
+	fs.Files["/sys/class/saradc/saradc_ch1"].Contents = "123\n"
+	i, _ = a.AnalogRead("37")
+	gobot.Assert(t, i, 123)
+
+	i, err := a.AnalogRead("9999")
+	gobot.Assert(t, err, errors.New("Not a valid pin"))
+
 }
 
 func TestODroidC1AdaptorI2c(t *testing.T) {
