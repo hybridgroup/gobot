601e4916a272a4e533d64755c263e9243cf5a93e
Matt Aimonetti <mattaimonetti@gmail.com>
2015-10-11 00:49:57 -0700

    let devs set and use custom LCD characters
    I noticed that the grove LCD screen only has an ASCII/Japanese font table. I need to be able to display accented characters
    looking at the data sheet the screen supports up to 8 custom CGRAM characters. I added an API to set those CGRAM characters
    as well as a map of common latin 1 characters + emoticons

--- examples/edison_grove_lcd.go
+++ examples/edison_grove_lcd.go
@@ -23,7 +23,10 @@ func main() {
 			screen.Clear()
 			screen.Home()
 			screen.SetRGB(0, 255, 0)
-			screen.Write("goodbye\nhave a nice day")
+			// set a custom character in the first position
+			screen.SetCustomChar(0, i2c.CustomLCDChars["smiley"])
+			// add the custom character at the end of the string
+			screen.Write("goodbye\nhave a nice day " + string(byte(0)))
 			gobot.Every(500*time.Millisecond, func() {
 				screen.Scroll(false)
 			})
