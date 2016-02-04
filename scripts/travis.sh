59cffa20fa5a90b4c60222b0d47bc370e8fb2f12
deadprogram <ron@hybridgroup.com>
2016-02-03 20:10:27 -0800

    What about -v

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -20,4 +20,4 @@ then
 fi
 
 export PATH=$PATH:$HOME/gopath/bin/
-goveralls -coverprofile=profile.cov -service=travis-ci -repotoken=sFrR9ZmLP5FLc34lOaqir67RPzYOvFPUB
+goveralls -v -coverprofile=profile.cov -service=travis-ci -repotoken=sFrR9ZmLP5FLc34lOaqir67RPzYOvFPUB
