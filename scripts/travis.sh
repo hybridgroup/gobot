bafe51e45df732dfe3617b31136d2098a7213de3
deadprogram <ron@hybridgroup.com>
2016-02-03 16:07:07 -0800

    Trying conditional build

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -2,14 +2,9 @@
 PACKAGES=('gobot' 'gobot/api' 'gobot/platforms/firmata/client' 'gobot/platforms/intel-iot/edison' 'gobot/sysfs' $(ls ./platforms | sed -e 's/^/gobot\/platforms\//'))
 EXITCODE=0
 
-go get code.google.com/p/go.tools/cmd/cover
+if ! go get code.google.com/p/go.tools/cmd/cover; then go get golang.org/x/tools/cmd/cover; fi
 
 go test -cover github.com/hybridgroup/gobot
-
-if [ $? -ne 0 ]
-then
-  go get golang.org/x/tools/cmd/cover
-fi
 
 echo "mode: set" > profile.cov
 touch tmp.cov
