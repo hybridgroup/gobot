f31e78cb8cf17c9ddb149ed591006dd5cc18ed86
deadprogram <ron@hybridgroup.com>
2016-02-03 19:37:27 -0800

    covermode=count

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -8,7 +8,7 @@ echo "mode: set" > profile.cov
 touch tmp.cov
 for package in "${PACKAGES[@]}"
 do
-  go test -a -coverprofile=tmp.cov github.com/hybridgroup/$package
+  go test -a -covermode=count -coverprofile=tmp.cov github.com/hybridgroup/$package
   if [ $? -ne 0 ]
   then
     EXITCODE=1
