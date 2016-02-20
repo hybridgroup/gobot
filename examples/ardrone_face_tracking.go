875ed071e9ec52fd8ee6185868e89380e91bdcea
deadprogram <ron@hybridgroup.com>
2016-02-19 18:39:30 -0800

    Update ARDrone face tracking example to use main go-opencv fork

--- examples/ardrone_face_tracking.go
+++ examples/ardrone_face_tracking.go
@@ -7,7 +7,7 @@ import (
 	"runtime"
 	"time"
 
-	cv "github.com/hybridgroup/go-opencv/opencv"
+	cv "github.com/lazywei/go-opencv/opencv"
 	"github.com/hybridgroup/gobot"
 	"github.com/hybridgroup/gobot/platforms/ardrone"
 	"github.com/hybridgroup/gobot/platforms/opencv"
