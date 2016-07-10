8db74052608ce79d5bbc0d2b995583af8b86eb07
deadprogram <ron@hybridgroup.com>
2016-07-10 12:37:58 -0600

    [ble] Implement Ollie Roll example
    Signed-off-by: deadprogram <ron@hybridgroup.com>

--- examples/ollie_roll.go
+++ examples/ollie_roll.go
@@ -3,7 +3,6 @@ package main
 import (
 	"os"
 	"time"
-	"fmt"
 
 	"github.com/hybridgroup/gobot"
 	"github.com/hybridgroup/gobot/platforms/ble"
@@ -17,17 +16,8 @@ func main() {
 
 	work := func() {
 		ollie.SetRGB(255, 0, 255)
-		gobot.After(1*time.Second, func() {
-			fmt.Println("fwd")
-			ollie.Roll(60, 0)
-		})
-		gobot.After(3*time.Second, func() {
-			fmt.Println("back")
-			ollie.Roll(60, 180)
-		})
-		gobot.After(5*time.Second, func() {
-			fmt.Println("stop")
-			ollie.Stop()
+		gobot.Every(3*time.Second, func() {
+			ollie.Roll(30, uint16(gobot.Rand(360)))
 		})
 	}
 
