c08968fc638f58931ff14218b945ab329ac9e7ff
deadprogram <ron@hybridgroup.com>
2016-08-25 23:57:16 +0200

    joule: adds pin mappings from the second header
    Signed-off-by: deadprogram <ron@hybridgroup.com>

--- platforms/intel-iot/joule/joule_adaptor.go
+++ platforms/intel-iot/joule/joule_adaptor.go
@@ -214,8 +214,209 @@ var sysfsPinMap = map[string]sysfsPin{
 		pwmPin: -1,
 	},
 
-	// TODO: Second header
+	// Second header
+	// GND
+	"41": sysfsPin{
+		pin:    -1,
+		pwmPin: -1,
+	},
+	// 5V
+	"42": sysfsPin{
+		pin:    -1,
+		pwmPin: -1,
+	},
+	// GND
+	"43": sysfsPin{
+		pin:    -1,
+		pwmPin: -1,
+	},
+	// 5V
+	"44": sysfsPin{
+		pin:    -1,
+		pwmPin: -1,
+	},
+	// GND
+	"45": sysfsPin{
+		pin:    -1,
+		pwmPin: -1,
+	},
+	// 3.3V
+	"46": sysfsPin{
+		pin:    -1,
+		pwmPin: -1,
+	},
+	// GND
+	"47": sysfsPin{
+		pin:    -1,
+		pwmPin: -1,
+	},
+	// 3.3V
+	"48": sysfsPin{
+		pin:    -1,
+		pwmPin: -1,
+	},
+	// GND
+	"49": sysfsPin{
+		pin:    -1,
+		pwmPin: -1,
+	},
+	// 1.8V
+	"50": sysfsPin{
+		pin:    -1,
+		pwmPin: -1,
+	},
+	// GPIO
+	"51": sysfsPin{
+		pin:    456,
+		pwmPin: -1,
+	},
+	// 1.8V
+	"52": sysfsPin{
+		pin:    -1,
+		pwmPin: -1,
+	},
+	// PANEL
+	"53": sysfsPin{
+		pin:    270,
+		pwmPin: -1,
+	},
+	// GND
+	"54": sysfsPin{
+		pin:    -1,
+		pwmPin: -1,
+	},
+	// PANEL
+	"55": sysfsPin{
+		pin:    271,
+		pwmPin: -1,
+	},
+	// CAMERA
+	"56": sysfsPin{
+		pin:    -1,
+		pwmPin: -1,
+	},
+	// PANEL
+	"57": sysfsPin{
+		pin:    272,
+		pwmPin: -1,
+	},
+	// CAMERA
+	"58": sysfsPin{
+		pin:    -1,
+		pwmPin: -1,
+	},
+	// SPP0FS0
+	"59": sysfsPin{
+		pin:    411,
+		pwmPin: -1,
+	},
+	// CAMERA
+	"60": sysfsPin{
+		pin:    -1,
+		pwmPin: -1,
+	},
+	// SPP0FS1
+	"61": sysfsPin{
+		pin:    412,
+		pwmPin: -1,
+	},
+	// SPI_DAT
+	"62": sysfsPin{
+		pin:    -1,
+		pwmPin: -1,
+	},
+	// SPP0FS2
+	"63": sysfsPin{
+		pin:    411,
+		pwmPin: -1,
+	},
+	// SPICLKB
+	"64": sysfsPin{
+		pin:    384,
+		pwmPin: -1,
+	},
+	// SPP0FS3
+	"65": sysfsPin{
+		pin:    410,
+		pwmPin: -1,
+	},
+	// SPICLKA
+	"66": sysfsPin{
+		pin:    383,
+		pwmPin: -1,
+	},
+	// SPP0TX
+	"67": sysfsPin{
+		pin:    414,
+		pwmPin: -1,
+	},
+	// UART0RX
+	"68": sysfsPin{
+		pin:    467,
+		pwmPin: -1,
+	},
+	// SPP0RX
+	"69": sysfsPin{
+		pin:    415,
+		pwmPin: -1,
+	},
+	// UART0RT
+	"70": sysfsPin{
+		pin:    469,
+		pwmPin: -1,
+	},
+	// I2C1SDA
+	"71": sysfsPin{
+		pin:    317,
+		pwmPin: -1,
+	},
+	// UART0CT
+	"72": sysfsPin{
+		pin:    412,
+		pwmPin: -1,
+	},
+	// I2C1SCL
+	"73": sysfsPin{
+		pin:    418,
+		pwmPin: -1,
+	},
+	// UART1TX
+	"74": sysfsPin{
+		pin:    484,
+		pwmPin: -1,
+	},
+	// I2C2SDA
+	"75": sysfsPin{
+		pin:    319,
+		pwmPin: -1,
+	},
+	// UART1RX
+	"76": sysfsPin{
+		pin:    483,
+		pwmPin: -1,
+	},
+	// I2C2SCL
+	"77": sysfsPin{
+		pin:    320,
+		pwmPin: -1,
+	},
+	// UART1RT
+	"78": sysfsPin{
+		pin:    485,
+		pwmPin: -1,
+	},
+	// RTC_CLK
+	"79": sysfsPin{
+		pin:    367,
+		pwmPin: -1,
+	},
+	// UART1CT
+	"80": sysfsPin{
+		pin:    486,
+		pwmPin: -1,
+	},
 
+	// Built-in LEDs
 	// LED100
 	"100": sysfsPin{
 		pin:    337,
