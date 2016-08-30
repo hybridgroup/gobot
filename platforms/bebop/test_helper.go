ef2f0bf203c68aa0fc5301b058652e008df7696b
deadprogram <ron@hybridgroup.com>
2016-08-30 20:24:47 +0200

    core: Refactor tests to allow 'metal' development using Gobot adaptors/drivers.
    Signed-off-by: deadprogram <ron@hybridgroup.com>

--- platforms/bebop/test_helper.go
+++ platforms/bebop/test_helper.go
@@ -3,20 +3,20 @@ package bebop
 type testDrone struct{}
 
 //func (t testDrone) Close() {}
-func (t testDrone) TakeOff() error { return nil }
-func (t testDrone) Land() error { return nil }
-func (t testDrone) Up(n int) error { return nil }
-func (t testDrone) Down(n int) error { return nil }
-func (t testDrone) Left(n int) error { return nil}
-func (t testDrone) Right(n int) error { return nil }
-func (t testDrone) Forward(n int) error { return nil }
-func (t testDrone) Backward(n int) error { return nil }
-func (t testDrone) Clockwise(n int) error { return nil }
-func (t testDrone) CounterClockwise(n int) error { return nil }
-func (t testDrone) Stop() error { return nil }
-func (t testDrone) Connect() error { return nil }
-func (t testDrone) Video() chan []byte { return nil }
-func (t testDrone) StartRecording() error { return nil }
-func (t testDrone) StopRecording() error { return nil }
+func (t testDrone) TakeOff() error                    { return nil }
+func (t testDrone) Land() error                       { return nil }
+func (t testDrone) Up(n int) error                    { return nil }
+func (t testDrone) Down(n int) error                  { return nil }
+func (t testDrone) Left(n int) error                  { return nil }
+func (t testDrone) Right(n int) error                 { return nil }
+func (t testDrone) Forward(n int) error               { return nil }
+func (t testDrone) Backward(n int) error              { return nil }
+func (t testDrone) Clockwise(n int) error             { return nil }
+func (t testDrone) CounterClockwise(n int) error      { return nil }
+func (t testDrone) Stop() error                       { return nil }
+func (t testDrone) Connect() error                    { return nil }
+func (t testDrone) Video() chan []byte                { return nil }
+func (t testDrone) StartRecording() error             { return nil }
+func (t testDrone) StopRecording() error              { return nil }
 func (t testDrone) HullProtection(protect bool) error { return nil }
-func (t testDrone) Outdoor(outdoor bool) error { return nil }
+func (t testDrone) Outdoor(outdoor bool) error        { return nil }
