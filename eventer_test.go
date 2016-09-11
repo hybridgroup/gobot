39b2ffd6fd395d04d7e049e177421e773570850e
deadprogram <ron@hybridgroup.com>
2016-09-11 13:27:43 +0200

    core: function DeleteEvent added to Eventer interface
    Signed-off-by: deadprogram <ron@hybridgroup.com>

--- eventer_test.go
+++ eventer_test.go
@@ -4,11 +4,21 @@ import (
 	"testing"
 )
 
-func TestEventer(t *testing.T) {
+func TestEventerAddEvent(t *testing.T) {
 	e := NewEventer()
 	e.AddEvent("test")
 
 	if _, ok := e.Events()["test"]; !ok {
 		t.Errorf("Could not add event to list of Event names")
+	}
+}
+
+func TestEventerDeleteEvent(t *testing.T) {
+	e := NewEventer()
+	e.AddEvent("test1")
+	e.DeleteEvent("test1")
+
+	if _, ok := e.Events()["test1"]; ok {
+		t.Errorf("Could not add delete event from list of Event names")
 	}
 }
