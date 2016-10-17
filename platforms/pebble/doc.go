dcd36192d1c8fde6aa4710a24e307dd75c4cee2a
Benjamin Radovsky <radovskyb@gmail.com>
2016-10-17 17:30:25 +1100

    Create doc.go

--- platforms/pebble/doc.go
+++ platforms/pebble/doc.go
@@ -1,56 +1,40 @@
 /*
 Package pebble contains the Gobot adaptor and driver for Pebble smart watch.
-
 Installing:
-
 It requires the 2.x iOS or Android app, and "watchbot" app (https://github.com/hybridgroup/watchbot)
 installed on Pebble watch. Then install running:
-
 	go get github.com/hybridgroup/gobot/platforms/pebble
-
 Example:
-
 Before running the example, make sure configuration settings match with your program. In the example, api host is your computer IP, robot name is 'pebble' and robot api port is 8080
-
 	package main
-
 	import (
 		"fmt"
-
 		"github.com/hybridgroup/gobot"
 		"github.com/hybridgroup/gobot/api"
 		"github.com/hybridgroup/gobot/platforms/pebble"
 	)
-
 	func main() {
-		gbot := gobot.NewGobot()
+		gbot := gobot.NewMaster()
 		api.NewAPI(gbot).Start()
-
-		pebbleAdaptor := pebble.NewPebbleAdaptor("pebble")
-		pebbleDriver := pebble.NewPebbleDriver(pebbleAdaptor, "pebble")
-
+		pebbleAdaptor := pebble.NewAdaptor()
+		watch := pebble.NewDriver(pebbleAdaptor)
 		work := func() {
-			pebbleDriver.SendNotification("Hello Pebble!")
-			pebbleDriver.On(pebbleDriver.Event("button"), func(data interface{}) {
+			watch.SendNotification("Hello Pebble!")
+			watch.On(watch.Event("button"), func(data interface{}) {
 				fmt.Println("Button pushed: " + data.(string))
 			})
-
-			pebbleDriver.On(pebbleDriver.Event("tap"), func(data interface{}) {
+			watch.On(watch.Event("tap"), func(data interface{}) {
 				fmt.Println("Tap event detected")
 			})
 		}
-
 		robot := gobot.NewRobot("pebble",
 			[]gobot.Connection{pebbleAdaptor},
-			[]gobot.Device{pebbleDriver},
+			[]gobot.Device{watch},
 			work,
 		)
-
 		gbot.AddRobot(robot)
-
 		gbot.Start()
 	}
-
 For more information refer to the pebble README:
 https://github.com/hybridgroup/gobot/blob/master/platforms/pebble/README.md
 */
