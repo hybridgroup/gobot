4ef57b6d7228577d6435ff4084601ffaad5a2abe
deadprogram <ron@hybridgroup.com>
2016-11-26 20:44:09 +0100

    docs: Update audio platform godocs
    Signed-off-by: deadprogram <ron@hybridgroup.com>

--- platforms/audio/audio_driver.go
+++ platforms/audio/audio_driver.go
@@ -1,5 +1,4 @@
-// Based on aplay audio adaptor written by @colemanserious (https://github.com/colemanserious)
-
+// Package audio is based on aplay audio adaptor written by @colemanserious (https://github.com/colemanserious)
 package audio
 
 import (
@@ -8,6 +7,7 @@ import (
 	"github.com/hybridgroup/gobot"
 )
 
+// Driver is gobot software device for audio playback
 type Driver struct {
 	name       string
 	connection gobot.Connection
@@ -18,8 +18,13 @@ type Driver struct {
 	filename string
 }
 
+// NewDriver returns a new audio Driver. It accepts:
+//
+// *Adaptor: The audio adaptor to use for the driver
+//  string: The filename of the audio to start playing
+//
 func NewDriver(a *Adaptor, filename string) *Driver {
-	d := &Driver{
+	return &Driver{
 		name:       "Audio",
 		connection: a,
 		interval:   500 * time.Millisecond,
@@ -28,23 +33,30 @@ func NewDriver(a *Adaptor, filename string) *Driver {
 		Eventer:    gobot.NewEventer(),
 		Commander:  gobot.NewCommander(),
 	}
-	return d
 }
 
+// Name returns the Driver Name
 func (d *Driver) Name() string { return d.name }
 
+// SetName sets the Driver Name
 func (d *Driver) SetName(n string) { d.name = n }
 
+// Filename returns the file name for the driver to playback
 func (d *Driver) Filename() string { return d.filename }
 
+// Connection returns the Driver Connection
 func (d *Driver) Connection() gobot.Connection {
 	return d.connection
 }
 
+// Sound plays back a sound file. It accepts:
+//
+//  string: The filename of the audio to start playing
 func (d *Driver) Sound(fileName string) []error {
 	return d.Connection().(*Adaptor).Sound(fileName)
 }
 
+// Play plays back the current sound file.
 func (d *Driver) Play() []error {
 	return d.Sound(d.Filename())
 }
@@ -53,10 +65,12 @@ func (d *Driver) adaptor() *Adaptor {
 	return d.Connection().(*Adaptor)
 }
 
+// Start starts the Driver
 func (d *Driver) Start() (err error) {
 	return
 }
 
+// Halt halts the Driver
 func (d *Driver) Halt() (err error) {
 	return
 }
