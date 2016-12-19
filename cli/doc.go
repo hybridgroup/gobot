316ebe1a601983735a63b8fa453a61425ce74104
deadprogram <ron@hybridgroup.com>
2016-12-19 11:22:41 +0100

    cli: a few small adjustments to the CLI and docs.
    Signed-off-by: deadprogram <ron@hybridgroup.com>

--- cli/doc.go
+++ cli/doc.go
@@ -7,16 +7,5 @@ CLI tool for generating new Gobot projects.
 	USAGE:
 		 gobot [global options] command [command options] [arguments...]
 
-	VERSION:
-		 0.12.1
-
-	COMMANDS:
-		 generate     Generate new Gobot adaptors, drivers, and platforms
-		 help, h      Shows a list of commands or help for one command
-
-	GLOBAL OPTIONS:
-		 --help, -h           show help
-		 --version, -v        print the version
-
 */
 package main
