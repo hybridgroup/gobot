ade09c734e513541fc9388558c50d7c276a3fd5c
deadprogram <ron@hybridgroup.com>
2016-12-21 10:56:51 +0100

    docs: correct I2C README link
    Signed-off-by: deadprogram <ron@hybridgroup.com>

--- drivers/i2c/doc.go
+++ drivers/i2c/doc.go
@@ -3,9 +3,9 @@ Package i2c provides Gobot drivers for i2c devices.
 
 Installing:
 
-	go get gobot.io/x/gobot/platforms/i2c
+	go get -d -u gobot.io/x/gobot
 
 For further information refer to i2c README:
-https://gobot.io/x/gobot/blob/master/drivers/i2c/README.md
+https://github.com/hybridgroup/gobot/blob/master/drivers/i2c/README.md
 */
-package i2c
+package i2c // import "gobot.io/x/gobot/drivers/i2c"
