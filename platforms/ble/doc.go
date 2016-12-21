73779ffc5416cd7077efd8d6eedfccf92a1b4193
deadprogram <ron@hybridgroup.com>
2016-12-21 10:51:08 +0100

    docs: correct BLE readme link
    Signed-off-by: deadprogram <ron@hybridgroup.com>

--- platforms/ble/doc.go
+++ platforms/ble/doc.go
@@ -2,6 +2,6 @@
 Package ble provides the Gobot adaptor for Bluetooth LE.
 
 For more information refer to the README:
-https://gobot.io/x/gobot/blob/master/platforms/ble/README.md
+https://github.com/hybridgroup/gobot/blob/master/platforms/ble/README.md
 */
 package ble // import "gobot.io/x/gobot/platforms/ble"
