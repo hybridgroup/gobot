8d995defee733192439d6640f66a5aa9b4d6cce6
deadprogram <ron@hybridgroup.com>
2016-12-21 10:53:31 +0100

    docs: correct MegaPi README link
    Signed-off-by: deadprogram <ron@hybridgroup.com>

--- platforms/megapi/doc.go
+++ platforms/megapi/doc.go
@@ -2,6 +2,6 @@
 Package megapi provides the Gobot adaptor for MegaPi.
 
 For more information refer to the README:
-https://gobot.io/x/gobot/blob/master/platforms/megapi/README.md
+https://github.com/hybridgroup/gobot/blob/master/platforms/megapi/README.md
 */
 package megapi // import "gobot.io/x/gobot/platforms/megapi"
