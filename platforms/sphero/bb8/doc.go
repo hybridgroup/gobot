924d9a16995039c3de0fa0180537015857933f45
deadprogram <ron@hybridgroup.com>
2016-12-21 10:55:42 +0100

    docs: correct BB-8 README link
    Signed-off-by: deadprogram <ron@hybridgroup.com>

--- platforms/sphero/bb8/doc.go
+++ platforms/sphero/bb8/doc.go
@@ -2,6 +2,6 @@
 Package bb8 contains the Gobot driver for the Sphero BB-8.
 
 For more information refer to the BB-8 README:
-https://gobot.io/x/gobot/blob/master/platforms/sphero/bb8/README.md
+https://github.com/hybridgroup/gobot/blob/master/platforms/sphero/bb8/README.md
 */
 package bb8 // import "gobot.io/x/gobot/platforms/sphero/bb8"
