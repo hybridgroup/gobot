f7265ae249714703470867da096ba580b5aeeb8b
deadprogram <ron@hybridgroup.com>
2016-12-21 10:50:36 +0100

    docs: correct audio readme link
    Signed-off-by: deadprogram <ron@hybridgroup.com>

--- platforms/audio/doc.go
+++ platforms/audio/doc.go
@@ -2,6 +2,6 @@
 Package audio provides the Gobot adaptor for audio.
 
 For more information refer to the README:
-https://gobot.io/x/gobot/blob/master/platforms/audio/README.md
+https://github.com/hybridgroup/gobot/blob/master/platforms/audio/README.md
 */
 package audio // import "gobot.io/x/gobot/platforms/audio"
