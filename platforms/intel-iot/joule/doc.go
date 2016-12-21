8f88c383baf1d9fba1dc1f89ebc6387047db5884
deadprogram <ron@hybridgroup.com>
2016-12-21 10:52:17 +0100

    docs: correct Joule README link
    Signed-off-by: deadprogram <ron@hybridgroup.com>

--- platforms/intel-iot/joule/doc.go
+++ platforms/intel-iot/joule/doc.go
@@ -2,6 +2,6 @@
 Package joule contains the Gobot adaptor for the Intel Joule.
 
 For further information refer to intel-iot README:
-https://gobot.io/x/gobot/blob/master/platforms/intel-iot/joule/README.md
+https://github.com/hybridgroup/gobot/blob/master/platforms/intel-iot/joule/README.md
 */
 package joule // import "gobot.io/x/gobot/platforms/intel-iot/joule"
