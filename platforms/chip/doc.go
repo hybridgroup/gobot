2c274bb6064f732a14d0757194f1d26f73fda400
deadprogram <ron@hybridgroup.com>
2016-12-21 10:51:24 +0100

    docs: correct C.H.I.P. readme link
    Signed-off-by: deadprogram <ron@hybridgroup.com>

--- platforms/chip/doc.go
+++ platforms/chip/doc.go
@@ -2,6 +2,6 @@
 Package chip contains the Gobot adaptor for the CHIP
 
 For further information refer to the chip README:
-https://gobot.io/x/gobot/blob/master/platforms/chip/README.md
+https://github.com/hybridgroup/gobot/blob/master/platforms/chip/README.md
 */
 package chip // import "gobot.io/x/gobot/platforms/chip"
