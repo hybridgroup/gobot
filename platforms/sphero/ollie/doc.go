2dd8dab78a9e87dc1b3596d8db62bae6ba8c1cfd
deadprogram <ron@hybridgroup.com>
2016-12-21 10:55:51 +0100

    docs: correct Ollie README link
    Signed-off-by: deadprogram <ron@hybridgroup.com>

--- platforms/sphero/ollie/doc.go
+++ platforms/sphero/ollie/doc.go
@@ -2,6 +2,6 @@
 Package ollie contains the Gobot driver for the Sphero Ollie.
 
 For more information refer to the Ollie README:
-https://gobot.io/x/gobot/blob/master/platforms/sphero/ollie/README.md
+https://github.com/hybridgroup/gobot/blob/master/platforms/sphero/ollie/README.md
 */
 package ollie // import "gobot.io/x/gobot/platforms/sphero/ollie"
