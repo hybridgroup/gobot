09e8cd6bd38831fc5837e0d0e245379b88a7b942
deadprogram <ron@hybridgroup.com>
2016-12-21 10:53:51 +0100

    docs: correct NATS README link
    Signed-off-by: deadprogram <ron@hybridgroup.com>

--- platforms/nats/doc.go
+++ platforms/nats/doc.go
@@ -3,6 +3,6 @@ Package nats provides Gobot adaptor for the nats message service.
 Installing:
   go get gobot.io/x/gobot/platforms/nats
 For further information refer to mqtt README:
-https://gobot.io/x/gobot/blob/master/platforms/nats/README.md
+https://github.com/hybridgroup/gobot/blob/master/platforms/nats/README.md
 */
 package nats // import "gobot.io/x/gobot/platforms/nats"
