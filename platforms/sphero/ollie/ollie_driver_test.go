9d9970c3b2a92eb82982058320c7c8185de141fa
deadprogram <ron@hybridgroup.com>
2016-12-23 09:36:47 +0100

    ollie: add very basic existance test
    Signed-off-by: deadprogram <ron@hybridgroup.com>

--- platforms/sphero/ollie/ollie_driver_test.go
+++ platforms/sphero/ollie/ollie_driver_test.go
@@ -1,5 +1,22 @@
 package ollie
 
-import "gobot.io/x/gobot"
+import (
+  "testing"
+
+  "gobot.io/x/gobot"
+  "gobot.io/x/gobot/gobottest"
+
+  "gobot.io/x/gobot/platforms/ble"
+)
 
 var _ gobot.Driver = (*Driver)(nil)
+
+func initTestOllieDriver() *Driver {
+	d := NewDriver(ble.NewClientAdaptor("D7:99:5A:26:EC:38"))
+	return d
+}
+
+func TestOllieDriver(t *testing.T) {
+	d := initTestOllieDriver()
+	gobottest.Assert(t, d.Name(), "Ollie")
+}
