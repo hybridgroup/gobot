3ece1f618c3e3c15b2ae553fc62eb1814bc24bb8
deadprogram <ron@hybridgroup.com>
2016-12-27 13:04:36 +0100

    minidrone: add minimal test
    Signed-off-by: deadprogram <ron@hybridgroup.com>

--- platforms/parrot/minidrone/minidrone_driver_test.go
+++ platforms/parrot/minidrone/minidrone_driver_test.go
@@ -1,5 +1,22 @@
 package minidrone
 
-import "gobot.io/x/gobot"
+import (
+  "testing"
+
+  "gobot.io/x/gobot"
+  "gobot.io/x/gobot/gobottest"
+
+  "gobot.io/x/gobot/platforms/ble"
+)
 
 var _ gobot.Driver = (*Driver)(nil)
+
+func initTestMinidroneDriver() *Driver {
+	d := NewDriver(ble.NewClientAdaptor("D7:99:5A:26:EC:38"))
+	return d
+}
+
+func TestMinidroneDriver(t *testing.T) {
+	d := initTestMinidroneDriver()
+	gobottest.Assert(t, d.Name(), "Minidrone")
+}
