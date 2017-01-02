80be4947150418fc8f4a6225461d6508a35db5d8
deadprogram <ron@hybridgroup.com>
2017-01-02 21:40:20 +0100

    firmata: add example using tcp connection to firmata
    Signed-off-by: deadprogram <ron@hybridgroup.com>

---
+++ examples/wifi_firmata_blink.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"os"
+	"time"
+
+	"gobot.io/x/gobot"
+	"gobot.io/x/gobot/drivers/gpio"
+	"gobot.io/x/gobot/platforms/firmata"
+)
+
+func main() {
+	firmataAdaptor := firmata.NewTCPAdaptor(os.Args[1])
+	led := gpio.NewLedDriver(firmataAdaptor, "13")
+
+	work := func() {
+		gobot.Every(1*time.Second, func() {
+			led.Toggle()
+		})
+	}
+
+	robot := gobot.NewRobot("bot",
+		[]gobot.Connection{firmataAdaptor},
+		[]gobot.Device{led},
+		work,
+	)
+
+	robot.Start()
+}
