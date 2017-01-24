9f0bc79dc247b66310e74cbc3b76196e8fe7424a
Durgesh Mankekar <durgesh@gmail.com>
2017-01-24 16:53:29 -0500

    bb8: Added example of collision detection

---
+++ examples/bb8-collision.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"gobot.io/x/gobot"
+	"gobot.io/x/gobot/platforms/ble"
+	"gobot.io/x/gobot/platforms/sphero/bb8"
+)
+
+func main() {
+	bleAdaptor := ble.NewClientAdaptor(os.Args[1])
+	bb := bb8.NewDriver(bleAdaptor)
+
+	work := func() {
+
+		bb.On("collision", func(data interface{}) {
+			fmt.Printf("collision detected = %+v \n", data)
+			bb.SetRGB(255, 0, 0)
+		})
+
+		bb.SetRGB(0, 255, 0)
+		bb.Roll(80, 0)
+	}
+
+	robot := gobot.NewRobot("bb8",
+		[]gobot.Connection{bleAdaptor},
+		[]gobot.Device{bb},
+		work,
+	)
+
+	robot.Start()
+
+}
