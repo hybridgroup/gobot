66f5b483f11ce796cbc0ecb11f431bbaeb295b79
deadprogram <ron@hybridgroup.com>
2017-01-30 11:48:15 +0100

    docs: modify metal example to range thru channel instead of select
    Signed-off-by: deadprogram <ron@hybridgroup.com>

--- examples/firmata_metal_button.go
+++ examples/firmata_metal_button.go
@@ -1,31 +1,35 @@
+// TO RUN:
+//	firmata_metal_button <PORT>
+//
+// EXAMPLE:
+//	go run ./examples/firmata_metal_button /dev/ttyACM0
+//
 package main
 
 import (
 	"fmt"
+	"os"
 
 	"gobot.io/x/gobot/drivers/gpio"
 	"gobot.io/x/gobot/platforms/firmata"
 )
 
 func main() {
-	f := firmata.NewAdaptor("/dev/ttyACM0")
+	f := firmata.NewAdaptor(os.Args[1])
 	f.Connect()
 
-	led := gpio.NewLedDriver(f, "13")
+	led := gpio.NewLedDriver(f, "2")
 	led.Start()
 	led.Off()
 
-	button := gpio.NewButtonDriver(f, "5")
+	button := gpio.NewButtonDriver(f, "3")
 	button.Start()
 
 	buttonEvents := button.Subscribe()
-	for {
-		select {
-		case event := <-buttonEvents:
-			fmt.Println("Event:", event.Name, event.Data)
-			if event.Name == gpio.ButtonPush {
-				led.Toggle()
-			}
+	for event := range buttonEvents {
+		fmt.Println("Event:", event.Name, event.Data)
+		if event.Name == gpio.ButtonPush {
+			led.Toggle()
 		}
 	}
 }
