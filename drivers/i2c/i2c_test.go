7df004dd4680225729fbaea450dea385200adf49
deadprogram <ron@hybridgroup.com>
2017-02-14 13:41:01 +0100

    i2c: a few more tests for Connection
    Signed-off-by: deadprogram <ron@hybridgroup.com>

--- drivers/i2c/i2c_test.go
+++ drivers/i2c/i2c_test.go
@@ -8,6 +8,11 @@ import (
 )
 
 func initI2CDevice() sysfs.I2cDevice {
+	fs := sysfs.NewMockFilesystem([]string{
+		"/dev/i2c-1",
+	})
+	sysfs.SetFilesystem(fs)
+
 	sysfs.SetSyscall(&sysfs.MockSyscall{})
 	i, _ := sysfs.NewI2cDevice("/dev/i2c-1")
 	return i
@@ -22,4 +27,52 @@ func TestI2CRead(t *testing.T) {
 	c := NewConnection(initI2CDevice(), 0x06)
 	i, _ := c.Read([]byte{})
 	gobottest.Assert(t, i, 0)
+}
+
+func TestI2CWrite(t *testing.T) {
+	c := NewConnection(initI2CDevice(), 0x06)
+	i, _ := c.Write([]byte{0x01})
+	gobottest.Assert(t, i, 1)
+}
+
+func TestI2CReadByte(t *testing.T) {
+	c := NewConnection(initI2CDevice(), 0x06)
+	v, _ := c.ReadByte()
+	gobottest.Assert(t, v, uint8(0))
+}
+
+func TestI2CReadByteData(t *testing.T) {
+	c := NewConnection(initI2CDevice(), 0x06)
+	v, _ := c.ReadByteData(0x01)
+	gobottest.Assert(t, v, uint8(0))
+}
+
+func TestI2CReadWordData(t *testing.T) {
+	c := NewConnection(initI2CDevice(), 0x06)
+	v, _ := c.ReadWordData(0x01)
+	gobottest.Assert(t, v, uint16(0))
+}
+
+func TestI2CReadBlockData(t *testing.T) {
+	c := NewConnection(initI2CDevice(), 0x06)
+	i, _ := c.ReadBlockData(0x01, []byte{})
+	gobottest.Assert(t, i, 0)
+}
+
+func TestI2CWriteByte(t *testing.T) {
+	c := NewConnection(initI2CDevice(), 0x06)
+	err := c.WriteByte(0x01)
+	gobottest.Assert(t, err, nil)
+}
+
+func TestI2CWriteByteData(t *testing.T) {
+	c := NewConnection(initI2CDevice(), 0x06)
+	err := c.WriteByteData(0x01, 0x01)
+	gobottest.Assert(t, err, nil)
+}
+
+func TestI2CWriteWordData(t *testing.T) {
+	c := NewConnection(initI2CDevice(), 0x06)
+	err := c.WriteWordData(0x01, 0x01)
+	gobottest.Assert(t, err, nil)
 }
