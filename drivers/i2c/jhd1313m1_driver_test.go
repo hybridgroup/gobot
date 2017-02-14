9a0dc0a0125fb2b9c4f9db456ec84669f45f5663
deadprogram <ron@hybridgroup.com>
2017-02-14 14:44:35 +0100

    jhd1313m1: add some test coverage
    Signed-off-by: deadprogram <ron@hybridgroup.com>

--- drivers/i2c/jhd1313m1_driver_test.go
+++ drivers/i2c/jhd1313m1_driver_test.go
@@ -1,6 +1,7 @@
 package i2c
 
 import (
+	"errors"
 	"strings"
 	"testing"
 
@@ -49,4 +50,59 @@ func TestJHD1313MDriverSetName(t *testing.T) {
 func TestJHD1313MDriverOptions(t *testing.T) {
 	d := NewJHD1313M1Driver(newI2cTestAdaptor(), WithBus(2))
 	gobottest.Assert(t, d.GetBusOrDefault(1), 2)
+}
+
+func TestJHD1313MDriverSetNameStart(t *testing.T) {
+	d := initTestJHD1313M1Driver()
+	gobottest.Assert(t, d.Start(), nil)
+}
+
+func TestJHD1313MDriverSetRgb(t *testing.T) {
+	d, _ := initTestJHD1313M1DriverWithStubbedAdaptor()
+	d.Start()
+	gobottest.Assert(t, d.SetRGB(0x00, 0x00, 0x00), nil)
+}
+
+func TestJHD1313MDriverClear(t *testing.T) {
+	d, _ := initTestJHD1313M1DriverWithStubbedAdaptor()
+	d.Start()
+	gobottest.Assert(t, d.Clear(), nil)
+}
+
+func TestJHD1313MDriverHome(t *testing.T) {
+	d, _ := initTestJHD1313M1DriverWithStubbedAdaptor()
+	d.Start()
+	gobottest.Assert(t, d.Home(), nil)
+}
+
+func TestJHD1313MDriverWrite(t *testing.T) {
+	d, _ := initTestJHD1313M1DriverWithStubbedAdaptor()
+	d.Start()
+	gobottest.Assert(t, d.Write("Hello"), nil)
+}
+
+func TestJHD1313MDriverSetPosition(t *testing.T) {
+	d, _ := initTestJHD1313M1DriverWithStubbedAdaptor()
+	d.Start()
+	gobottest.Assert(t, d.SetPosition(2), nil)
+}
+
+func TestJHD1313MDriverScroll(t *testing.T) {
+	d, _ := initTestJHD1313M1DriverWithStubbedAdaptor()
+	d.Start()
+	gobottest.Assert(t, d.Scroll(true), nil)
+}
+
+func TestJHD1313MDriverSetCustomChar(t *testing.T) {
+	d, _ := initTestJHD1313M1DriverWithStubbedAdaptor()
+	data := [8]byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}
+	d.Start()
+	gobottest.Assert(t, d.SetCustomChar(0, data), nil)
+}
+
+func TestJHD1313MDriverSetCustomCharError(t *testing.T) {
+	d, _ := initTestJHD1313M1DriverWithStubbedAdaptor()
+	data := [8]byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}
+	d.Start()
+	gobottest.Assert(t, d.SetCustomChar(10, data), errors.New("can't set a custom character at a position greater than 7"))
 }
