69bba81747bcd09b4ecb79b03733574d7162119e
deadprogram <ron@hybridgroup.com>
2017-02-16 13:20:04 +0100

    test: testing use of codecov.io for code coverage reporting
    Signed-off-by: deadprogram <ron@hybridgroup.com>

--- ci/travis.sh
+++ ci/travis.sh
@@ -2,8 +2,8 @@
 PACKAGES=('gobot' 'gobot/api' 'gobot/sysfs' 'gobot/drivers/gpio' 'gobot/drivers/aio' 'gobot/drivers/i2c' 'gobot/platforms/firmata/client' 'gobot/platforms/intel-iot/edison' 'gobot/platforms/intel-iot/joule' 'gobot/platforms/parrot/ardrone' 'gobot/platforms/parrot/bebop' 'gobot/platforms/parrot/minidrone' 'gobot/platforms/sphero/ollie' 'gobot/platforms/sphero/bb8' $(ls ./platforms | sed -e 's/^/gobot\/platforms\//'))
 EXITCODE=0
 
-echo "mode: set" > profile.cov
-touch tmp.cov
+echo "" > coverage.txt
+
 for package in "${PACKAGES[@]}"
 do
   go test -a -coverprofile=tmp.cov gobot.io/x/$package
@@ -11,13 +11,6 @@ do
   then
     EXITCODE=1
   fi
-  cat tmp.cov | grep -v "mode: set" >> profile.cov
+  cat tmp.cov >> coverage.txt
+  rm tmp.cov
 done
-
-if [ $EXITCODE -ne 0 ]
-then
-  exit $EXITCODE
-fi
-
-export PATH=$PATH:$HOME/gopath/bin/
-goveralls -v -coverprofile=profile.cov -service=travis-ci -repotoken=sFrR9ZmLP5FLc34lOaqir67RPzYOvFPUB
