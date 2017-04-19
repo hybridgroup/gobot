b82fa229950e0e3cc2ba77a4cce0ca47d0662be8
bezineb5 <bben.pub@laposte.net>
2017-04-18 21:35:48 -0400

    Better range definition for the angle

--- drivers/gpio/angular_servo_driver.go
+++ drivers/gpio/angular_servo_driver.go
@@ -9,6 +9,7 @@ type AngularServoDriver struct {
 	connection PwmWriter
 	gobot.Commander
 	CurrentAngle float64
+	MinAngle     float64
 	MaxAngle     float64
 	minPeriod    float64
 	maxPeriod    float64
@@ -21,16 +22,21 @@ type AngularServoDriver struct {
 //	"Min" - See ServoDriver.Min
 //	"Center" - See ServoDriver.Center
 //	"Max" - See ServoDriver.Max
-func NewAngularServoDriver(a PwmWriter, pin string, maxAngle float64, minPeriod float64, maxPeriod float64) *AngularServoDriver {
+func NewAngularServoDriver(a PwmWriter, pin string, options ...func(*AngularServoDriver)) *AngularServoDriver {
 	s := &AngularServoDriver{
 		name:         gobot.DefaultName("AngularServo"),
 		connection:   a,
 		pin:          pin,
 		Commander:    gobot.NewCommander(),
-		CurrentAngle: 0,
-		MaxAngle:     maxAngle,
-		minPeriod:    minPeriod,
-		maxPeriod:    maxPeriod,
+		CurrentAngle: 0.0,
+		MinAngle:     0.0,
+		MaxAngle:     180.0,
+		minPeriod:    1.0 / 1000.0,
+		maxPeriod:    2.0 / 1000.0,
+	}
+
+	for _, option := range options {
+		option(s)
 	}
 
 	s.AddCommand("Move", func(params map[string]interface{}) interface{} {
@@ -48,6 +54,33 @@ func NewAngularServoDriver(a PwmWriter, pin string, maxAngle float64, minPeriod 
 	})
 
 	return s
+}
+
+// WithServoAngles sets the range of acceptable angles.
+// Note that the min can be less than the max to reverse servo operation.
+func (s *AngularServoDriver) WithServoAngles(minAngle float64, maxAngle float64) {
+	s.MinAngle = minAngle
+	s.MaxAngle = maxAngle
+}
+
+// WithServoAngles sets the range of acceptable angles.
+func WithServoAngles(minAngle float64, maxAngle float64) func(*AngularServoDriver) {
+	return func(s *AngularServoDriver) {
+		s.WithServoAngles(minAngle, maxAngle)
+	}
+}
+
+// WithPeriodRange sets the range of acceptable periods.
+func (s *AngularServoDriver) WithPeriodRange(minPeriod float64, maxPeriod float64) {
+	s.minPeriod = minPeriod
+	s.maxPeriod = maxPeriod
+}
+
+// WithPeriodRange sets the range of acceptable periods.
+func WithPeriodRange(minPeriod float64, maxPeriod float64) func(*AngularServoDriver) {
+	return func(s *AngularServoDriver) {
+		s.WithPeriodRange(minPeriod, maxPeriod)
+	}
 }
 
 // Name returns the ServoDrivers name
@@ -68,24 +101,26 @@ func (s *AngularServoDriver) Start() (err error) { return }
 // Halt implements the Driver interface
 func (s *AngularServoDriver) Halt() (err error) { return }
 
-// Move sets the servo to the specified angle. Acceptable angles are 0-180
+// Move sets the servo to the specified angle. Acceptable angles are defined at setup time
 func (s *AngularServoDriver) Move(angle float64) (err error) {
-	if !(angle >= 0 && angle <= s.MaxAngle) {
+	if !((angle >= s.MinAngle && angle <= s.MaxAngle) ||
+		(angle <= s.MinAngle && angle >= s.MaxAngle)) {
+		// We need to support the case were minAngle>maxAngle, to reverse the servo
 		return ErrServoOutOfRange
 	}
 	s.CurrentAngle = angle
-	period := s.minPeriod + (s.maxPeriod-s.minPeriod)*angle/s.MaxAngle
+	period := s.minPeriod + (s.maxPeriod-s.minPeriod)*(angle-s.MinAngle)/(s.MaxAngle-s.MinAngle)
 	return s.connection.SetPwmPeriod(s.Pin(), period)
 }
 
 // Min sets the servo to it's minimum position
 func (s *AngularServoDriver) Min() (err error) {
-	return s.Move(0)
+	return s.Move(s.MinAngle)
 }
 
 // Center sets the servo to it's center position
 func (s *AngularServoDriver) Center() (err error) {
-	return s.Move(s.MaxAngle / 2.0)
+	return s.Move((s.MaxAngle - s.MinAngle) / 2.0)
 }
 
 // Max sets the servo to its maximum position
