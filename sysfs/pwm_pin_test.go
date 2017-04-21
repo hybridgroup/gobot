73a85aecdf15860b1e40e38ab5b700e44f640de2
deadprogram <ron@hybridgroup.com>
2017-04-21 10:05:59 +0200

    sysfs: increase test coverage for pwm pin
    Signed-off-by: deadprogram <ron@hybridgroup.com>

--- sysfs/pwm_pin_test.go
+++ sysfs/pwm_pin_test.go
@@ -38,6 +38,11 @@ func TestPwmPin(t *testing.T) {
 	fs.Files["/sys/class/pwm/pwmchip0/pwm10/period"].Contents = "6"
 	data, _ := pin.Period()
 	gobottest.Assert(t, data, "6")
+
+	gobottest.Refute(t, fs.Files["/sys/class/pwm/pwmchip0/pwm10/duty_cycle"].Contents, "1")
+	err = pin.WriteDuty("100")
+	gobottest.Assert(t, err, nil)
+	gobottest.Assert(t, fs.Files["/sys/class/pwm/pwmchip0/pwm10/duty_cycle"].Contents, "100")
 }
 
 func TestPwmPinExportError(t *testing.T) {
@@ -76,4 +81,24 @@ func TestPwmPinUnxportError(t *testing.T) {
 	}
 
 	gobottest.Refute(t, pin.Unexport(), nil)
+}
+
+func TestPwmPinPeriodError(t *testing.T) {
+	fs := NewMockFilesystem([]string{
+		"/sys/class/pwm/pwmchip0/export",
+		"/sys/class/pwm/pwmchip0/unexport",
+		"/sys/class/pwm/pwmchip0/pwm10/enable",
+		"/sys/class/pwm/pwmchip0/pwm10/period",
+		"/sys/class/pwm/pwmchip0/pwm10/duty_cycle",
+	})
+
+	SetFilesystem(fs)
+
+	pin := NewPwmPin(10)
+	pin.read = func(string) ([]byte, error) {
+		return nil, &os.PathError{Err: syscall.EBUSY}
+	}
+
+	_, err := pin.Period()
+	gobottest.Refute(t, err, nil)
 }
