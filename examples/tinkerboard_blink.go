8ca2e5927145b0d82ef0088b20f3bbd6a2e2f338
deadprogram <ron@hybridgroup.com>
2017-04-26 15:54:59 +0200

    tinkerboard: add examples for ASUS Tinker Board
    Signed-off-by: deadprogram <ron@hybridgroup.com>

---
+++ examples/tinkerboard_blink.go
@@ -0,0 +1,32 @@
+// +build example
+//
+// Do not build by default.
+
+package main
+
+import (
+	"time"
+
+	"gobot.io/x/gobot"
+	"gobot.io/x/gobot/drivers/gpio"
+	"gobot.io/x/gobot/platforms/tinkerboard"
+)
+
+func main() {
+	r := tinkerboard.NewAdaptor()
+	led := gpio.NewLedDriver(r, "7")
+
+	work := func() {
+		gobot.Every(1*time.Second, func() {
+			led.Toggle()
+		})
+	}
+
+	robot := gobot.NewRobot("blinkBot",
+		[]gobot.Connection{r},
+		[]gobot.Device{led},
+		work,
+	)
+
+	robot.Start()
+}
