028ef8183d82cbef27b135c46c8ace10131be9ed
deadprogram <ron@hybridgroup.com>
2017-04-29 13:40:27 +0200

    nats: minor increase test coverage for driver
    Signed-off-by: deadprogram <ron@hybridgroup.com>

--- platforms/nats/nats_driver_test.go
+++ platforms/nats/nats_driver_test.go
@@ -15,6 +15,7 @@ func TestNatsDriver(t *testing.T) {
 
 	gobottest.Assert(t, strings.HasPrefix(d.Name(), "NATS"), true)
 	gobottest.Assert(t, strings.HasPrefix(d.Connection().Name(), "NATS"), true)
+	gobottest.Refute(t, d.adaptor(), nil)
 
 	gobottest.Assert(t, d.Start(), nil)
 	gobottest.Assert(t, d.Halt(), nil)
@@ -25,4 +26,10 @@ func TestNatsDriverName(t *testing.T) {
 	gobottest.Assert(t, strings.HasPrefix(d.Name(), "NATS"), true)
 	d.SetName("NewName")
 	gobottest.Assert(t, d.Name(), "NewName")
+}
+
+func TestNatsDriverTopic(t *testing.T) {
+	d := NewDriver(initTestNatsAdaptor(), "/test/topic")
+	d.SetTopic("interestingtopic")
+	gobottest.Assert(t, d.Topic(), "interestingtopic")
 }
