f4d7c220a3c67c5b9b0e31e2558694e13bd9d6f9
deadprogram <ron@hybridgroup.com>
2017-05-06 08:18:04 +0200

    joule: add examples using Joule with ADS1015 ADC
    Signed-off-by: deadprogram <ron@hybridgroup.com>

---
+++ examples/joule_led_brightness_with_analog_input.go
@@ -0,0 +1,39 @@
+// +build example
+//
+// Do not build by default.
+
+package main
+
+import (
+	"fmt"
+
+	"gobot.io/x/gobot"
+	"gobot.io/x/gobot/drivers/aio"
+	"gobot.io/x/gobot/drivers/gpio"
+	"gobot.io/x/gobot/drivers/i2c"
+	"gobot.io/x/gobot/platforms/intel-iot/joule"
+)
+
+func main() {
+	e := joule.NewAdaptor()
+	ads1015 := i2c.NewADS1015Driver(e)
+	sensor := aio.NewAnalogSensorDriver(ads1015, "0")
+	led := gpio.NewLedDriver(e, "J12_26")
+
+	work := func() {
+		sensor.On(aio.Data, func(data interface{}) {
+			brightness := uint8(gobot.ToScale(gobot.FromScale(float64(data.(int)), 0, 1023), 0, 255))
+			fmt.Println("sensor", data)
+			fmt.Println("brightness", brightness)
+			led.Brightness(brightness)
+		})
+	}
+
+	robot := gobot.NewRobot("sensorBot",
+		[]gobot.Connection{e},
+		[]gobot.Device{ads1015, sensor, led},
+		work,
+	)
+
+	robot.Start()
+}
