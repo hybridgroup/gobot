44dec6d800b42e7b9da8bc612266cdb480954f41
Erik Agsjö <erik.agsjo@gmail.com>
2017-06-04 00:13:49 +0200

    Updated Tinkerboard and sysfs tests to updated PWM polarity contract
    Signed-off-by: Erik Agsjö <erik.agsjo@gmail.com>

--- platforms/tinkerboard/adaptor.go
+++ platforms/tinkerboard/adaptor.go
@@ -174,13 +174,17 @@ func (c *Adaptor) PWMPin(pin string) (sysfsPin sysfs.PWMPinner, err error) {
 		if err = newPin.Export(); err != nil {
 			return
 		}
+		// Make sure pwm is disabled when setting polarity
+		if err = newPin.Enable(false); err != nil {
+			return
+		}
+		if err = newPin.InvertPolarity(false); err != nil {
+			return
+		}
 		if err = newPin.Enable(true); err != nil {
 			return
 		}
 		if err = newPin.SetPeriod(10000000); err != nil {
-			return
-		}
-		if err = newPin.InvertPolarity(false); err != nil {
 			return
 		}
 		c.pwmPins[i] = newPin
