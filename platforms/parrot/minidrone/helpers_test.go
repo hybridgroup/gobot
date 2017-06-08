776923f099cd61de29613f6cc5e10c225ca1e72b
deadprogram <ron@hybridgroup.com>
2017-06-08 13:48:51 +0200

    minidrone: never expect responses for characteristic writes
    Signed-off-by: deadprogram <ron@hybridgroup.com>

--- platforms/parrot/minidrone/helpers_test.go
+++ platforms/parrot/minidrone/helpers_test.go
@@ -9,21 +9,22 @@ import (
 var _ ble.BLEConnector = (*bleTestClientAdaptor)(nil)
 
 type bleTestClientAdaptor struct {
-	name    string
-	address string
-	mtx     sync.Mutex
-
+	name                    string
+	address                 string
+	mtx                     sync.Mutex
+	withoutReponses         bool
 	testReadCharacteristic  func(string) ([]byte, error)
 	testWriteCharacteristic func(string, []byte) error
 }
 
-func (t *bleTestClientAdaptor) Connect() (err error)    { return }
-func (t *bleTestClientAdaptor) Reconnect() (err error)  { return }
-func (t *bleTestClientAdaptor) Disconnect() (err error) { return }
-func (t *bleTestClientAdaptor) Finalize() (err error)   { return }
-func (t *bleTestClientAdaptor) Name() string            { return t.name }
-func (t *bleTestClientAdaptor) SetName(n string)        { t.name = n }
-func (t *bleTestClientAdaptor) Address() string         { return t.address }
+func (t *bleTestClientAdaptor) Connect() (err error)     { return }
+func (t *bleTestClientAdaptor) Reconnect() (err error)   { return }
+func (t *bleTestClientAdaptor) Disconnect() (err error)  { return }
+func (t *bleTestClientAdaptor) Finalize() (err error)    { return }
+func (t *bleTestClientAdaptor) Name() string             { return t.name }
+func (t *bleTestClientAdaptor) SetName(n string)         { t.name = n }
+func (t *bleTestClientAdaptor) Address() string          { return t.address }
+func (t *bleTestClientAdaptor) WithoutReponses(use bool) { t.withoutReponses = use }
 
 func (t *bleTestClientAdaptor) ReadCharacteristic(cUUID string) (data []byte, err error) {
 	t.mtx.Lock()
