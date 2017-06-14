30345764c7e7f5287985f4444f9bb07fd57f16d5
deadprogram <ron@hybridgroup.com>
2017-06-14 11:15:42 +0200

    curie: Accelerometer, Gyroscope, and Temperature sensors implemented
    Signed-off-by: deadprogram <ron@hybridgroup.com>

---
+++ examples/firmata_curie_imu.go
@@ -0,0 +1,53 @@
+// +build example
+//
+// Do not build by default.
+
+package main
+
+import (
+	"log"
+	"time"
+
+	"gobot.io/x/gobot"
+	"gobot.io/x/gobot/drivers/gpio"
+	"gobot.io/x/gobot/platforms/firmata"
+	"gobot.io/x/gobot/platforms/intel-iot/curie"
+)
+
+func main() {
+	firmataAdaptor := firmata.NewAdaptor("/dev/ttyACM0")
+	led := gpio.NewLedDriver(firmataAdaptor, "13")
+	imu := curie.NewIMUDriver(firmataAdaptor)
+
+	work := func() {
+		imu.On("Accelerometer", func(data interface{}) {
+			log.Println("Accelerometer", data)
+		})
+
+		imu.On("Gyroscope", func(data interface{}) {
+			log.Println("Gyroscope", data)
+		})
+
+		imu.On("Temperature", func(data interface{}) {
+			log.Println("Temperature", data)
+		})
+
+		gobot.Every(1*time.Second, func() {
+			led.Toggle()
+		})
+
+		gobot.Every(100*time.Millisecond, func() {
+			imu.ReadAccelerometer()
+			imu.ReadGyroscope()
+			imu.ReadTemperature()
+		})
+	}
+
+	robot := gobot.NewRobot("blinkmBot",
+		[]gobot.Connection{firmataAdaptor},
+		[]gobot.Device{imu, led},
+		work,
+	)
+
+	robot.Start()
+}
--- platforms/intel-iot/curie/imu_driver.go
+++ platforms/intel-iot/curie/imu_driver.go
@@ -1,9 +1,34 @@
 package curie
 
 import (
+	"errors"
+
 	"gobot.io/x/gobot"
 	"gobot.io/x/gobot/platforms/firmata"
 )
+
+const (
+	CURIE_IMU              = 0x11
+	CURIE_IMU_READ_ACCEL   = 0x00
+	CURIE_IMU_READ_GYRO    = 0x01
+	CURIE_IMU_READ_TEMP    = 0x02
+	CURIE_IMU_SHOCK_DETECT = 0x03
+	CURIE_IMU_STEP_COUNTER = 0x04
+	CURIE_IMU_TAP_DETECT   = 0x05
+	CURIE_IMU_READ_MOTION  = 0x06
+)
+
+type AccelerometerData struct {
+	X int16
+	Y int16
+	Z int16
+}
+
+type GyroscopeData struct {
+	X int16
+	Y int16
+	Z int16
+}
 
 // IMUDriver represents the IMU that is built-in to the Curie
 type IMUDriver struct {
@@ -25,6 +50,25 @@ func NewIMUDriver(a *firmata.Adaptor) *IMUDriver {
 
 // Start starts up the IMUDriver
 func (imu *IMUDriver) Start() (err error) {
+	imu.connection.On("SysexResponse", func(res interface{}) {
+		data := res.([]byte)
+		if data[1] == CURIE_IMU {
+			switch data[2] {
+			case CURIE_IMU_READ_ACCEL:
+				val, _ := parseAccelerometerData(data)
+				imu.Publish("Accelerometer", val)
+
+			case CURIE_IMU_READ_GYRO:
+				val, _ := parseGyroscopeData(data)
+				imu.Publish("Gyroscope", val)
+
+			case CURIE_IMU_READ_TEMP:
+				val, _ := parseTemperatureData(data)
+				imu.Publish("Temperature", val)
+
+			}
+		}
+	})
 	return
 }
 
@@ -41,3 +85,56 @@ func (imu *IMUDriver) SetName(n string) { imu.name = n }
 
 // Connection returns the IMUDriver's Connection
 func (imu *IMUDriver) Connection() gobot.Connection { return imu.connection }
+
+// ReadAccelerometer calls the Curie's built-in accelerometer. The result will
+// be returned by the Sysex response message
+func (imu *IMUDriver) ReadAccelerometer() error {
+	return imu.connection.WriteSysex([]byte{CURIE_IMU, CURIE_IMU_READ_ACCEL})
+}
+
+// ReadGyroscope calls the Curie's built-in gyroscope. The result will
+// be returned by the Sysex response message
+func (imu *IMUDriver) ReadGyroscope() error {
+	return imu.connection.WriteSysex([]byte{CURIE_IMU, CURIE_IMU_READ_GYRO})
+}
+
+// ReadTemperature calls the Curie's built-in temperature sensor.
+// The result will be returned by the Sysex response message
+func (imu *IMUDriver) ReadTemperature() error {
+	return imu.connection.WriteSysex([]byte{CURIE_IMU, CURIE_IMU_READ_TEMP})
+}
+
+func parseAccelerometerData(data []byte) (*AccelerometerData, error) {
+	if len(data) < 9 {
+		return nil, errors.New("Invalid data")
+	}
+	x := int16(uint16(data[3]) | uint16(data[4])<<7)
+	y := int16(uint16(data[5]) | uint16(data[6])<<7)
+	z := int16(uint16(data[7]) | uint16(data[8])<<7)
+
+	res := &AccelerometerData{X: x, Y: y, Z: z}
+	return res, nil
+}
+
+func parseGyroscopeData(data []byte) (*GyroscopeData, error) {
+	if len(data) < 9 {
+		return nil, errors.New("Invalid data")
+	}
+	x := int16(uint16(data[3]) | uint16(data[4])<<7)
+	y := int16(uint16(data[5]) | uint16(data[6])<<7)
+	z := int16(uint16(data[7]) | uint16(data[8])<<7)
+
+	res := &GyroscopeData{X: x, Y: y, Z: z}
+	return res, nil
+}
+
+func parseTemperatureData(data []byte) (float32, error) {
+	if len(data) < 8 {
+		return 0, errors.New("Invalid data")
+	}
+	t1 := int16(uint16(data[3]) | uint16(data[4])<<7)
+	t2 := int16(uint16(data[5]) | uint16(data[6])<<7)
+
+	res := (float32(t1+(t2*8)) / 512.0) + 23.0
+	return res, nil
+}
