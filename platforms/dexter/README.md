40fc1a2411b4c726eaa8a3ca144735f5714458d9
deadprogram <ron@hybridgroup.com>
2017-09-14 14:16:20 +0200

    gopigo3: moving GoPiGo3 into separate subpackage of Dexter to make room for more robots
    Signed-off-by: deadprogram <ron@hybridgroup.com>

---
+++ platforms/dexter/README.md
@@ -0,0 +1,6 @@
+# Dexter
+
+This package contains the Gobot drivers for the various Dexter Industries (https://www.dexterindustries.com) robots.
+
+This package currently supports the following robots:
+- [GoPiGo3](https://www.dexterindustries.com/gopigo3/)
---
+++ platforms/dexter/dexter.go
@@ -0,0 +1,10 @@
+/*
+Package dexter contains Gobot drivers for the Dexter Industries robots
+
+This package currently supports the following robots:
+- GoPiGo3
+
+For further information refer to Dexter README:
+https://gobot.io/x/gobot/blob/master/platforms/dexter/README.md
+*/
+package dexter
