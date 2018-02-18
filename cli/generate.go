2657e572dc38efcc1b2c8fd091bac3fb3bfe69fa
Ron Evans <ron@hybridgroup.com>
2018-02-18 22:29:28 +0100

    cli: remove extra newline
    Signed-off-by: Ron Evans <ron@hybridgroup.com>

--- cli/generate.go
+++ cli/generate.go
@@ -31,7 +31,7 @@ func Generate() cli.Command {
 				}
 			}
 			if valid == false {
-				fmt.Println("Invalid/no subcommand supplied.\n")
+				fmt.Println("Invalid/no subcommand supplied.")
 				fmt.Println("Usage:")
 				fmt.Println(" gobot generate adaptor <name> [package] # generate a new Gobot adaptor")
 				fmt.Println(" gobot generate driver  <name> [package] # generate a new Gobot driver")
