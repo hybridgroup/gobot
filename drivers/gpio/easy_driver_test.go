66b61fab2ff0f16d5c7bdd66a6c4da3c18ed4944
Brendan Stennett <brendan@thinkdataworks.com>
2018-09-20 14:02:36 -0400

    Added some missing tests to increase coverage

--- drivers/gpio/easy_driver_test.go
+++ drivers/gpio/easy_driver_test.go
@@ -12,8 +12,16 @@ const (
 	stepsPerRev = 720
 )
 
+var adapter *gpioTestAdaptor
+
 func initEasyDriver() *EasyDriver {
-	return NewEasyDriver(newGpioTestAdaptor(), stepAngle, "1", "2", "3", "4")
+	adapter = newGpioTestAdaptor()
+	return NewEasyDriver(adapter, stepAngle, "1", "2", "3", "4")
+}
+
+func TestEasyDriver_Connection(t *testing.T) {
+	d := initEasyDriver()
+	gobottest.Assert(t, d.Connection(), adapter)
 }
 
 func TestEasyDriverDefaultName(t *testing.T) {
@@ -25,6 +33,20 @@ func TestEasyDriverSetName(t *testing.T) {
 	d := initEasyDriver()
 	d.SetName("OtherDriver")
 	gobottest.Assert(t, strings.HasPrefix(d.Name(), "OtherDriver"), true)
+}
+
+func TestEasyDriverStart(t *testing.T) {
+	d := initEasyDriver()
+	d.Start()
+	// noop - no error occurred
+}
+
+func TestEasyDriverHalt(t *testing.T) {
+	d := initEasyDriver()
+	d.Run()
+	gobottest.Assert(t, d.IsMoving(), true)
+	d.Halt()
+	gobottest.Assert(t, d.IsMoving(), false)
 }
 
 func TestEasyDriverMove(t *testing.T) {
@@ -75,6 +97,13 @@ func TestEasyDriverSetDirection(t *testing.T) {
 	gobottest.Assert(t, d.dir, int8(1))
 }
 
+func TestEasyDriverSetDirectionNoPin(t *testing.T) {
+	d := initEasyDriver()
+	d.dirPin = ""
+	err := d.SetDirection("cw")
+	gobottest.Refute(t, err, nil)
+}
+
 func TestEasyDriverSetSpeed(t *testing.T) {
 	d := initEasyDriver()
 	gobottest.Assert(t, d.rpm, uint(stepsPerRev/4)) // default speed of 720/4
@@ -105,6 +134,15 @@ func TestEasyDriverSleep(t *testing.T) {
 	gobottest.Assert(t, d.IsMoving(), false)
 }
 
+func TestEasyDriverSleepNoPin(t *testing.T) {
+	d := initEasyDriver()
+	d.sleepPin = ""
+	err := d.Sleep()
+	gobottest.Refute(t, err, nil)
+	err = d.Wake()
+	gobottest.Refute(t, err, nil)
+}
+
 func TestEasyDriverWake(t *testing.T) {
 	// let's test basic functionality
 	d := initEasyDriver()
@@ -112,6 +150,24 @@ func TestEasyDriverWake(t *testing.T) {
 	gobottest.Assert(t, d.IsSleeping(), true)
 	d.Wake()
 	gobottest.Assert(t, d.IsSleeping(), false)
+}
+
+func TestEasyDriverEnable(t *testing.T) {
+	// let's test basic functionality
+	d := initEasyDriver()
+	d.Disable()
+	gobottest.Assert(t, d.IsEnabled(), false)
+	d.Enable()
+	gobottest.Assert(t, d.IsEnabled(), true)
+}
+
+func TestEasyDriverEnableNoPin(t *testing.T) {
+	d := initEasyDriver()
+	d.enPin = ""
+	err := d.Disable()
+	gobottest.Refute(t, err, nil)
+	err = d.Enable()
+	gobottest.Refute(t, err, nil)
 }
 
 func TestEasyDriverDisable(t *testing.T) {
@@ -128,11 +184,3 @@ func TestEasyDriverDisable(t *testing.T) {
 	gobottest.Assert(t, d.IsMoving(), false)
 }
 
-func TestEasyDriverEnable(t *testing.T) {
-	// let's test basic functionality
-	d := initEasyDriver()
-	d.Disable()
-	gobottest.Assert(t, d.IsEnabled(), false)
-	d.Enable()
-	gobottest.Assert(t, d.IsEnabled(), true)
-}
