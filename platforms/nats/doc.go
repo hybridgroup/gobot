36730e33cf1edb9e211ecbe50aad6da7d6b00bbe
kvartborg <hello@kvartborg.net>
2019-06-11 11:47:02 +0200

    Fix typo
    Signed-off-by: kvartborg <hello@kvartborg.net>

--- platforms/nats/doc.go
+++ platforms/nats/doc.go
@@ -2,7 +2,7 @@
 Package nats provides Gobot adaptor for the nats message service.
 Installing:
   go get gobot.io/x/gobot/platforms/nats
-For further information refer to mqtt README:
+For further information refer to nats README:
 https://github.com/hybridgroup/gobot/blob/master/platforms/nats/README.md
 */
 package nats // import "gobot.io/x/gobot/platforms/nats"
