8a4c0e124fb0acfe76e774e3d8fb6e6ea55163b5
Jordi Íñigo <jordi.inyigo@gmail.com>
2022-05-28 00:07:59 +0200

    JSON adapted

--- platforms/joystick/joystick_dualsense.go
+++ platforms/joystick/joystick_dualsense.go
@@ -31,20 +31,20 @@ var dualsenseConfig = joystickConfig{
 	},
 	Buttons: []pair{
 		{
-			Name: "triangle",
-			ID:   3,
-		},
-		{
-			Name: "square",
-			ID:   2,
+			Name: "x",
+			ID:   0,
 		},
 		{
 			Name: "circle",
 			ID:   1,
 		},
 		{
-			Name: "x",
-			ID:   0,
+			Name: "square",
+			ID:   2,
+		},
+		{
+			Name: "triangle",
+			ID:   3,
 		},
 		{
 			Name: "create",
@@ -61,10 +61,6 @@ var dualsenseConfig = joystickConfig{
 		{
 			Name: "r2",
 			ID:   7,
-		},
-		{
-			Name: "share",
-			ID:   8,
 		},
 		{
 			Name: "l1",
@@ -95,31 +91,5 @@ var dualsenseConfig = joystickConfig{
 			ID:   15,
 		},
 	},
-	Hats: []hat{
-		{
-			Hat:  0,
-			Name: "down",
-			ID:   4,
-		},
-		{
-			Hat:  0,
-			Name: "up",
-			ID:   1,
-		},
-		{
-			Hat:  0,
-			Name: "left",
-			ID:   8,
-		},
-		{
-			Hat:  0,
-			Name: "right",
-			ID:   2,
-		},
-		{
-			Hat:  0,
-			Name: "released",
-			ID:   0,
-		},
-	},
+	Hats: []hat{},
 }
