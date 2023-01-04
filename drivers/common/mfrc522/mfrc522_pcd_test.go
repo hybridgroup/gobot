e27a33a59c2d22d090a7a0f9686e2ac00c4f5ee1
Thomas Kohler <g2t@e.mail.de>
2023-01-04 19:28:01 +0100

    add tests and README entry

--- drivers/common/mfrc522/mfrc522_pcd_test.go
+++ drivers/common/mfrc522/mfrc522_pcd_test.go
@@ -8,7 +8,8 @@ import (
 
 type busConnMock struct {
 	written []byte
-	simRead byte
+	readIdx int
+	simRead []byte
 	fifoIdx int
 	simFifo []byte
 }
@@ -23,7 +24,11 @@ func (c *busConnMock) ReadByteData(reg uint8) (uint8, error) {
 		c.fifoIdx++
 		return c.simFifo[c.fifoIdx-1], nil
 	default:
-		return c.simRead, nil
+		if len(c.simRead) > 0 {
+			c.readIdx++
+			return c.simRead[c.readIdx-1], nil
+		}
+		return 0, nil
 	}
 }
 
@@ -31,6 +36,13 @@ func (c *busConnMock) WriteByteData(reg uint8, data byte) error {
 	c.written = append(c.written, reg)
 	c.written = append(c.written, data)
 	return nil
+}
+
+func initTestMFRC522CommonWithStubbedConnector() (*MFRC522Common, *busConnMock) {
+	c := &busConnMock{}
+	d := NewMFRC522Common()
+	d.connection = c
+	return d, c
 }
 
 func TestNewMFRC522Common(t *testing.T) {
@@ -61,12 +73,10 @@ func TestInitialize(t *testing.T) {
 
 func Test_getVersion(t *testing.T) {
 	// arrange
-	c := &busConnMock{}
-	d := NewMFRC522Common()
-	d.connection = c
+	d, c := initTestMFRC522CommonWithStubbedConnector()
 	wantWritten := []byte{0x37}
 	const want = uint8(5)
-	c.simRead = want
+	c.simRead = []byte{want}
 	// act
 	got, err := d.getVersion()
 	// assert
@@ -105,10 +115,8 @@ func Test_switchAntenna(t *testing.T) {
 	for name, tc := range tests {
 		t.Run(name, func(t *testing.T) {
 			// arrange
-			c := &busConnMock{}
-			d := NewMFRC522Common()
-			d.connection = c
-			c.simRead = tc.simRead
+			d, c := initTestMFRC522CommonWithStubbedConnector()
+			c.simRead = []byte{tc.simRead}
 			// act
 			err := d.switchAntenna(tc.target)
 			// assert
@@ -120,10 +128,8 @@ func Test_switchAntenna(t *testing.T) {
 
 func Test_stopCrypto1(t *testing.T) {
 	// arrange
-	c := &busConnMock{}
-	d := NewMFRC522Common()
-	d.connection = c
-	c.simRead = 0xFF
+	d, c := initTestMFRC522CommonWithStubbedConnector()
+	c.simRead = []byte{0xFF}
 	wantWritten := []byte{0x08, 0x08, 0xF7}
 	// act
 	err := d.stopCrypto1()
@@ -132,11 +138,60 @@ func Test_stopCrypto1(t *testing.T) {
 	gobottest.Assert(t, c.written, wantWritten)
 }
 
+func Test_communicateWithPICC(t *testing.T) {
+	// arrange
+	d, c := initTestMFRC522CommonWithStubbedConnector()
+	dataToFifo := []byte{0xF1, 0xF2}
+	writtenPrepare := []byte{0x02, 0xF7, 0x04, 0x7F, 0x0A, 0x80, 0x01, 0x00}
+	writtenWriteFifo := []byte{0x09, 0xF1, 0x09, 0xF2}
+	writtenTransceive := []byte{0x0D, 0x00, 0x01, 0x0C}
+	writtenBitFramingStart := []byte{0x0D, 0x0D, 0x80}
+	writtenWaitAndFinish := []byte{0x04, 0x0D, 0x0D, 0x7F, 0x06}
+	writtenReadFifo := []byte{0x0A, 0x09, 0x09, 0x0C}
+	// read: bit framing register set, simulate calculation done, bit framing register clear, simulate no error,
+	// simulate control register 0x00
+	c.simRead = []byte{0x00, 0x30, 0xFF, 0x00, 0x00}
+	c.simFifo = []byte{0x11, 0x22}
+
+	backData := []byte{0x00, 0x00}
+	// act
+	// transceive, all 8 bits, no CRC
+	err := d.communicateWithPICC(0x0C, dataToFifo, backData, 0x00, false)
+	// assert
+	gobottest.Assert(t, err, nil)
+	gobottest.Assert(t, c.written[:8], writtenPrepare)
+	gobottest.Assert(t, c.written[8:12], writtenWriteFifo)
+	gobottest.Assert(t, c.written[12:16], writtenTransceive)
+	gobottest.Assert(t, c.written[16:19], writtenBitFramingStart)
+	gobottest.Assert(t, c.written[19:24], writtenWaitAndFinish)
+	gobottest.Assert(t, c.written[24:], writtenReadFifo)
+	gobottest.Assert(t, backData, []byte{0x11, 0x22})
+}
+
+func Test_calculateCRC(t *testing.T) {
+	// arrange
+	d, c := initTestMFRC522CommonWithStubbedConnector()
+	dataToFifo := []byte{0x02, 0x03}
+	writtenPrepare := []byte{0x01, 0x00, 0x05, 0x04, 0x0A, 0x80}
+	writtenFifo := []byte{0x09, 0x02, 0x09, 0x03}
+	writtenCalc := []byte{0x01, 0x03, 0x05, 0x01, 0x00}
+	writtenGetResult := []byte{0x22, 0x21}
+	c.simRead = []byte{0x04, 0x11, 0x22} // calculation done, crcL, crcH
+	gotCrcBack := []byte{0x00, 0x00}
+	// act
+	err := d.calculateCRC(dataToFifo, gotCrcBack)
+	// assert
+	gobottest.Assert(t, err, nil)
+	gobottest.Assert(t, c.written[:6], writtenPrepare)
+	gobottest.Assert(t, c.written[6:10], writtenFifo)
+	gobottest.Assert(t, c.written[10:15], writtenCalc)
+	gobottest.Assert(t, c.written[15:], writtenGetResult)
+	gobottest.Assert(t, gotCrcBack, []byte{0x11, 0x22})
+}
+
 func Test_writeFifo(t *testing.T) {
 	// arrange
-	c := &busConnMock{}
-	d := NewMFRC522Common()
-	d.connection = c
+	d, c := initTestMFRC522CommonWithStubbedConnector()
 	dataToFifo := []byte{0x11, 0x22, 0x33}
 	wantWritten := []byte{0x09, 0x11, 0x09, 0x22, 0x09, 0x33}
 	// act
@@ -148,9 +203,7 @@ func Test_writeFifo(t *testing.T) {
 
 func Test_readFifo(t *testing.T) {
 	// arrange
-	c := &busConnMock{}
-	d := NewMFRC522Common()
-	d.connection = c
+	d, c := initTestMFRC522CommonWithStubbedConnector()
 	c.simFifo = []byte{0x30, 0x20, 0x10}
 	wantWritten := []byte{0x0A, 0x09, 0x09, 0x09, 0x0C}
 	backData := []byte{0x00, 0x00, 0x00}
