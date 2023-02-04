f6bcb17ce4cde2cb6432459b8f0f6248d0098439
Thomas Kohler <g2t@e.mail.de>
2023-02-04 14:42:57 +0100

    add NanoPi NEO platform

---
+++ platforms/nanopi/doc.go
@@ -0,0 +1,7 @@
+/*
+Package nanopi contains the Gobot adaptor for the FriendlyARM NanoPi Boards.
+
+For further information refer to nanopi README:
+https://github.com/hybridgroup/gobot/blob/master/platforms/nanopi/README.md
+*/
+package nanopi // import "gobot.io/x/gobot/platforms/nanopi"
