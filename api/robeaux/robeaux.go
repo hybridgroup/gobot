1175db315df0c0210e02dce0bf7a6428fb27bd27
dlstjq7685 <31182898+dlstjq7685@users.noreply.github.com>
2023-06-11 02:25:01 +0900

    all: substitute deprecated ioutil methods (#923)

--- api/robeaux/robeaux.go
+++ api/robeaux/robeaux.go
@@ -45,7 +45,6 @@ import (
 	"compress/gzip"
 	"fmt"
 	"io"
-	"io/ioutil"
 	"os"
 	"path/filepath"
 	"strings"
@@ -1014,7 +1013,7 @@ func RestoreAsset(dir, name string) error {
 	if err != nil {
 		return err
 	}
-	err = ioutil.WriteFile(_filePath(dir, name), data, info.Mode())
+	err = os.WriteFile(_filePath(dir, name), data, info.Mode())
 	if err != nil {
 		return err
 	}
