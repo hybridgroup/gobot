293dd6bdb90fd0d8b0b95a5b398ec5e38a8d044f
Thomas Kohler <g2t@e.mail.de>
2023-06-19 15:42:01 +0200

    fix

--- system/syscall.go
+++ system/syscall.go
@@ -20,7 +20,7 @@ const (
 // nativeSyscall represents the native Syscall
 type nativeSyscall struct{}
 
-// Syscall calls the native syscall.Syscall, implements the SystemCaller interface
+// Syscall calls the native unix.Syscall, implements the SystemCaller interface
 func (sys *nativeSyscall) syscall(trap uintptr, f File, signal uintptr, payload unsafe.Pointer) (r1, r2 uintptr, err SyscallErrno) {
 	r1, r2, errNo := unix.Syscall(trap, f.Fd(), signal, uintptr(payload))
 	return r1, r2, SyscallErrno(errNo)
